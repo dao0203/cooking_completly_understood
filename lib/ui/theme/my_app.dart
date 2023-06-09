916c5d3e0467c6c028f6df0d2540389a4db8abf7
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-09 21:08:34 +0900

    [update] フォルダ・ファイル構造を更新

---
+++ lib/ui/theme/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:cooking_completly_understood/ui/view/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
