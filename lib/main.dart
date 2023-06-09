62e158bab37662d56b9ff7ae906994e8d8bba5e7
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-09 21:23:49 +0900

    [update] MyAppをProviderScopeでラップしてriverpodを使用できるようにする。

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-
 import 'package:cooking_completly_understood/ui/theme/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(
-    MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
