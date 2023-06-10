9a72bd67b0b77293ddb55fedd88a6fa3373314a0
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-10 20:15:57 +0900

    [update] ファイル名を変更

--- lib/di/position_data_source_providier.dart
+++ lib/di/position_data_source_providier.dart
@@ -1,5 +1,5 @@
 
-import 'package:cooking_completly_understood/data/sources/location_data_source.dart';
+import 'package:cooking_completly_understood/data/sources/position_data_source.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final positionDataSourceProvider = Provider<PositionDataSource>((ref) {
