8f6cfe0df04df3c9bd6ac7e4d58d5ae662cf03e6
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-11 00:45:08 +0900

    [add] WeatherInfoDataSourceのDataSourceデータクラスを追加

---
+++ lib/data/sources/weather_info_data_source.dart
@@ -0,0 +1,15 @@
+import 'package:chopper/chopper.dart';
+
+part 'weather_info_data_source.chopper.dart';
+
+@ChopperApi(baseUrl: 'https://api.open-meteo.com')
+abstract class WeatherInfoDataSource extends ChopperService {
+  static WeatherInfoDataSource create() =>
+      _$WeatherInfoDataSource(ChopperClient());
+
+  @Get(path: '/v1/forecast?latitude={latitude}&longitude={longtitude}&current_weather=true')
+  Future<Response> getCurrentWeather(
+    @Path('latitude') double latitude,
+    @Path("longtitude") double longtitude,
+  );
+}
