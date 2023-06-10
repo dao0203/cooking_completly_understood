3a8fca98bac87c2119a250a668f6527e45d0c3a4
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-11 00:54:21 +0900

    [add] 天気情報データソースクラスの単体テストを作成：成功

---
+++ test/weather_info_data_source_test.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:cooking_completly_understood/data/models/weather_forecast.dart';
+import 'package:cooking_completly_understood/data/sources/weather_info_data_source.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('クエリが正しく返されてきているかテスト', () async {
+    // ここにテストコードを書く
+    final weatherInfoDataSource = WeatherInfoDataSource.create();
+
+    final response = await weatherInfoDataSource.getCurrentWeather(
+      //東京の緯度経度
+      35.6804,
+      139.7690,
+    );
+    
+    //レスポンスボディを出力：成功
+    debugPrint(response.body);
+
+    //レスポンスボディをパース：成功
+    final weatherForecast =
+        WeatherForecast.fromJson(json.decode(response.body));
+
+    //パースしたデータの温度を出力
+    debugPrint(weatherForecast.currentWeather.temperature.toString());
+    expect(
+      weatherForecast.currentWeather.temperature.toString(),
+      isNotEmpty,
+    );
+  });
+}
