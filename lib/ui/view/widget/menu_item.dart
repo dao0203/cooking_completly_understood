83f512788920c3f26e78ea42e5dd24cdd6701a47
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-06-10 20:18:14 +0900

    メニューカード作成

---
+++ lib/ui/view/widget/menu_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Text('menu名'),
+    );
+  }
+}
