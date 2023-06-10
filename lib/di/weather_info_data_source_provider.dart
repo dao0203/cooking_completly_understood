d3e3d939521a389093233b1e6e71d5459a30047b
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-11 01:56:23 +0900

    [fix] タイポミスの修正

--- lib/di/weather_info_data_source_provider.dart
+++ lib/di/weather_info_data_source_provider.dart
@@ -2,6 +2,6 @@
 import 'package:cooking_completly_understood/data/sources/weather_info_data_source.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final weaatherInfoDataSourceProvider = Provider<WeatherInfoDataSource>((ref) {
+final weatherInfoDataSourceProvider = Provider<WeatherInfoDataSource>((ref) {
   return WeatherInfoDataSource.create();
 });
