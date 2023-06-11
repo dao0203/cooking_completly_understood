095409a49cef48d18a550a2b77dad4bc867e2492
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-11 16:08:48 +0900

    [add] MessageRepositoryクラスのCHATGPTにメッセージを送り、返ってきたメッセージがnullではないかの単体テストを実装（成功）

--- test/units/message_repository/send_message_and_receive_message_is_not_null_test.dart
+++ test/units/message_repository/send_message_and_receive_message_is_not_null_test.dart
@@ -1,28 +1,39 @@
 import 'package:cooking_completly_understood/data/repositories/message_repository.dart';
+import 'package:cooking_completly_understood/data/sources/message_service.dart';
 import 'package:cooking_completly_understood/data/sources/position_data_source.dart';
 import 'package:cooking_completly_understood/data/sources/weather_info_data_source.dart';
+import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 
-import '../mocks/posisition_data_source_mock.dart';
+import '../../mocks/posisition_data_source_mock.dart';
+
 
 @GenerateMocks([PositionDataSource])
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   late PositionDataSource positionDataSource;
+  // .evnから環境変数を読み込む
+  await dotenv.load(fileName: '.env');
+  // OpenAIのAPIキーを設定
+  OpenAI.apiKey = dotenv.get('OPEN_AI_API_KEY');
+
   setUp(() {
     // ここでモックを使うように設定
     positionDataSource = PositionDataSourceMock();
   });
   test(
-    'MessageRepositryのgetMessageが現状はしっかり気温が正しく出力されているかチェック',
+    '東京駅の気温と受け取ったメッセージからCHATGPTの返信を受け取る単体テスト',
     () async {
       final messageRepository = MessageRepository(
-        positionDataSource,
+        positionDataSource, //東京の緯度経度から天気情報を取得するようなモックを代入
         WeatherInfoDataSource.create(),
+        MessageService(),
       );
-      await messageRepository.getMessage().then(
+      const sendedMessage = "きゅうりと鶏肉のおすすめのレシピは？";
+      await messageRepository.sendMessageAndReceiveMessage(sendedMessage).then(
         (value) {
           debugPrint(value);
           expect(value, isNotNull);
