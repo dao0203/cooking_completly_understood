5c9ab26f8712bcc12ab3163dc1dc560115582949
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-06-11 13:10:54 +0900

    [add] レシピの詳細ページ作成

--- lib/ui/view/widget/menu_item.dart
+++ lib/ui/view/widget/menu_item.dart
@@ -1,3 +1,4 @@
+import 'package:cooking_completly_understood/ui/view/screens/menu_details_screen.dart';
 import 'package:flutter/material.dart';
 
 class MenuItem extends StatelessWidget {
@@ -5,33 +6,42 @@ class MenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width*0.9,
-        child: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '肉じゃが',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return const MenuDetailsScreen();
+            },
+          ),
+        );
+      },
+      child: Card(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('肉じゃが',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.timer_rounded),
+                    Text('10分'),
+                  ],
                 ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.timer_rounded),
-                  Text('10分'),
-                ],
-              ),
-
-              Row(
-                children: [
-                  Icon(Icons.calendar_month_rounded),
-                  Text('2023/1/1'),
-                ],
-              ),
-            ],
+                Row(
+                  children: [
+                    Icon(Icons.calendar_month_rounded),
+                    Text('2023/1/1'),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
