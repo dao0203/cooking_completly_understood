8faee26cb4193e9674249370c6653524abc32474
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-12 16:42:10 +0900

    [add] 将来的に変更する箇所をコメントで使用

--- lib/ui/state/message_state.dart
+++ lib/ui/state/message_state.dart
@@ -8,11 +8,16 @@ part 'message_state.g.dart';
 class MessagesState extends _$MessagesState {
   @override
   List<OpenAIChatCompletionChoiceMessageModel> build() {
+    //MessageStateが初期化された際に初期メッセージを送信する
     ref.read(messageRepositoryProvider).sendInitialMessage();
+    //TODO: MessageRepositoryからローカルDBに保存されているメッセージを取得してこちらに格納
     return [];
   }
 
   Future<void> sendMessageAndReceiveMessage(String message) async {
+
+    //TODO: ローカルDBにメッセージを保存する
+    //現状はstateに保存しているが、ローカルDBに保存するようにする
     state = [
       ...state,
       OpenAIChatCompletionChoiceMessageModel(
@@ -20,10 +25,15 @@ class MessagesState extends _$MessagesState {
         role: OpenAIChatMessageRole.user,
       ),
     ];
+
+    //TODO: MessageRepositoryからメッセージを送信
     await ref
         .read(messageRepositoryProvider)
         .sendMessageAndReceiveMessage(message)
         .then(
+          //現状はString型のメッセージを受け取って、stateに保存しているが、
+          //ここのクラスでは、このように状態を保存せず、repository内のビジネスロジックで
+          //ローカルDBに保存するように実装する
           (value) => state = [
             ...state,
             value,
