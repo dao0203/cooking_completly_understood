4eca07060c478b743aac77d04112f8a4467abe3a
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-14 03:57:10 +0900

    [update] isarから取得したデータを不変データクラスに変換してStream型で返すようにした

--- lib/data/repositories/recipe_repository.dart
+++ lib/data/repositories/recipe_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cooking_completly_understood/data/models/immu_recipe/immu_recipe.dart';
 import 'package:cooking_completly_understood/data/models/message/message.dart';
 import 'package:cooking_completly_understood/data/models/recipe/recipe.dart';
@@ -9,16 +11,19 @@ class RecipeRepository {
 
   // レシピを保存するメソッド
   Future<void> insertRecipeFromMessage(Message message) async {
-
     //保存するレシピデータクラスを作成
     final insertedRecipe = Recipe();
     //レシピデータクラスに値を代入
     insertedRecipe.name = message.recipeName;
     insertedRecipe.description = message.recipeDescription;
-    insertedRecipe.ingredientName = message.recipeIngredients.map((e) => e.name).toList();
-    insertedRecipe.ingredientQuantity = message.recipeIngredients.map((e) => e.quantity).toList();
-    insertedRecipe.stepNumber = message.recipeSteps.map((e) => e.number).toList();
-    insertedRecipe.stepDescription = message.recipeSteps.map((e) => e.description).toList();
+    insertedRecipe.ingredientName =
+        message.recipeIngredients.map((e) => e.name).toList();
+    insertedRecipe.ingredientQuantity =
+        message.recipeIngredients.map((e) => e.quantity).toList();
+    insertedRecipe.stepNumber =
+        message.recipeSteps.map((e) => e.number).toList();
+    insertedRecipe.stepDescription =
+        message.recipeSteps.map((e) => e.description).toList();
     insertedRecipe.calorie = message.nutrition.calorie;
     insertedRecipe.protein = message.nutrition.protein;
     insertedRecipe.fat = message.nutrition.fat;
@@ -29,33 +34,36 @@ class RecipeRepository {
   }
 
   // レシピを全て取得するメソッド
-  Future<List<ImmuRecipe>> getAllRecipes() async {
+  Stream<List<ImmuRecipe>> getAllRecipes() {
     //レシピを全て取得
-    final recipes = await _recipeService.getAllRecipes();
-    //レシピデータクラスのリストを作成
-    final immuRecipes = <ImmuRecipe>[];
-    //レシピデータクラスのリストに値を代入
-    for (final recipe in recipes) {
-      immuRecipes.add(
-        ImmuRecipe(
-          id: recipe.id,
-          name: recipe.name,
-          description: recipe.description,
-          ingredientName: recipe.ingredientName,
-          ingredientQuantity: recipe.ingredientQuantity,
-          stepNumber: recipe.stepNumber,
-          stepDescription: recipe.stepDescription,
-          calorie: recipe.calorie,
-          protein: recipe.protein,
-          fat: recipe.fat,
-          carbohydrate: recipe.carbohydrate,
-          salt: recipe.salt,
-        ),
-      );
-    }
-    //レシピデータクラスのリストを返す
-    return immuRecipes;
+    final recipes = _recipeService.getAllRecipes();
+
+    //レシピをImmuRecipeに変換する
+    //この機能デカすぎんか？
+    StreamTransformer<List<Recipe>, List<ImmuRecipe>> transformer =
+        StreamTransformer.fromHandlers(
+      handleData: (recipes, sink) {
+        List<ImmuRecipe> immuRecipes = recipes.map((e) {
+          return ImmuRecipe(
+            name: e.name,
+            description: e.description,
+            ingredientName: e.ingredientName,
+            ingredientQuantity: e.ingredientQuantity,
+            stepNumber: e.stepNumber,
+            stepDescription: e.stepDescription,
+            calorie: e.calorie,
+            protein: e.protein,
+            fat: e.fat,
+            carbohydrate: e.carbohydrate,
+            salt: e.salt,
+          );
+        }).toList();
+
+        sink.add(immuRecipes);
+      },
+    );
+
+    //レシピをImmuRecipeに変換して返す
+    return recipes.transform(transformer);
   }
-
-
 }
