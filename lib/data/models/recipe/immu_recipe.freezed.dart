d921f4320376e3a5b33acd3088a0fd3315220fc3
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-13 18:43:27 +0900

    [add] 不変なRecipeデータクラスを作成

---
+++ lib/data/models/recipe/immu_recipe.freezed.dart
@@ -0,0 +1,383 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'immu_recipe.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$ImmuRecipe {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  List<String> get ingredientName => throw _privateConstructorUsedError;
+  List<String> get ingredientQuantity => throw _privateConstructorUsedError;
+  List<String> get stepNumber => throw _privateConstructorUsedError;
+  List<String> get stepDescription => throw _privateConstructorUsedError;
+  String get calorie => throw _privateConstructorUsedError;
+  String get protein => throw _privateConstructorUsedError;
+  String get fat => throw _privateConstructorUsedError;
+  String get carbohydrate => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ImmuRecipeCopyWith<ImmuRecipe> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ImmuRecipeCopyWith<$Res> {
+  factory $ImmuRecipeCopyWith(
+          ImmuRecipe value, $Res Function(ImmuRecipe) then) =
+      _$ImmuRecipeCopyWithImpl<$Res, ImmuRecipe>;
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String description,
+      List<String> ingredientName,
+      List<String> ingredientQuantity,
+      List<String> stepNumber,
+      List<String> stepDescription,
+      String calorie,
+      String protein,
+      String fat,
+      String carbohydrate});
+}
+
+/// @nodoc
+class _$ImmuRecipeCopyWithImpl<$Res, $Val extends ImmuRecipe>
+    implements $ImmuRecipeCopyWith<$Res> {
+  _$ImmuRecipeCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? description = null,
+    Object? ingredientName = null,
+    Object? ingredientQuantity = null,
+    Object? stepNumber = null,
+    Object? stepDescription = null,
+    Object? calorie = null,
+    Object? protein = null,
+    Object? fat = null,
+    Object? carbohydrate = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      ingredientName: null == ingredientName
+          ? _value.ingredientName
+          : ingredientName // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      ingredientQuantity: null == ingredientQuantity
+          ? _value.ingredientQuantity
+          : ingredientQuantity // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      stepNumber: null == stepNumber
+          ? _value.stepNumber
+          : stepNumber // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      stepDescription: null == stepDescription
+          ? _value.stepDescription
+          : stepDescription // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      calorie: null == calorie
+          ? _value.calorie
+          : calorie // ignore: cast_nullable_to_non_nullable
+              as String,
+      protein: null == protein
+          ? _value.protein
+          : protein // ignore: cast_nullable_to_non_nullable
+              as String,
+      fat: null == fat
+          ? _value.fat
+          : fat // ignore: cast_nullable_to_non_nullable
+              as String,
+      carbohydrate: null == carbohydrate
+          ? _value.carbohydrate
+          : carbohydrate // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ImmuRecipeCopyWith<$Res>
+    implements $ImmuRecipeCopyWith<$Res> {
+  factory _$$_ImmuRecipeCopyWith(
+          _$_ImmuRecipe value, $Res Function(_$_ImmuRecipe) then) =
+      __$$_ImmuRecipeCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String description,
+      List<String> ingredientName,
+      List<String> ingredientQuantity,
+      List<String> stepNumber,
+      List<String> stepDescription,
+      String calorie,
+      String protein,
+      String fat,
+      String carbohydrate});
+}
+
+/// @nodoc
+class __$$_ImmuRecipeCopyWithImpl<$Res>
+    extends _$ImmuRecipeCopyWithImpl<$Res, _$_ImmuRecipe>
+    implements _$$_ImmuRecipeCopyWith<$Res> {
+  __$$_ImmuRecipeCopyWithImpl(
+      _$_ImmuRecipe _value, $Res Function(_$_ImmuRecipe) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? description = null,
+    Object? ingredientName = null,
+    Object? ingredientQuantity = null,
+    Object? stepNumber = null,
+    Object? stepDescription = null,
+    Object? calorie = null,
+    Object? protein = null,
+    Object? fat = null,
+    Object? carbohydrate = null,
+  }) {
+    return _then(_$_ImmuRecipe(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      ingredientName: null == ingredientName
+          ? _value._ingredientName
+          : ingredientName // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      ingredientQuantity: null == ingredientQuantity
+          ? _value._ingredientQuantity
+          : ingredientQuantity // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      stepNumber: null == stepNumber
+          ? _value._stepNumber
+          : stepNumber // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      stepDescription: null == stepDescription
+          ? _value._stepDescription
+          : stepDescription // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      calorie: null == calorie
+          ? _value.calorie
+          : calorie // ignore: cast_nullable_to_non_nullable
+              as String,
+      protein: null == protein
+          ? _value.protein
+          : protein // ignore: cast_nullable_to_non_nullable
+              as String,
+      fat: null == fat
+          ? _value.fat
+          : fat // ignore: cast_nullable_to_non_nullable
+              as String,
+      carbohydrate: null == carbohydrate
+          ? _value.carbohydrate
+          : carbohydrate // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ImmuRecipe implements _ImmuRecipe {
+  const _$_ImmuRecipe(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required final List<String> ingredientName,
+      required final List<String> ingredientQuantity,
+      required final List<String> stepNumber,
+      required final List<String> stepDescription,
+      required this.calorie,
+      required this.protein,
+      required this.fat,
+      required this.carbohydrate})
+      : _ingredientName = ingredientName,
+        _ingredientQuantity = ingredientQuantity,
+        _stepNumber = stepNumber,
+        _stepDescription = stepDescription;
+
+  @override
+  final int id;
+  @override
+  final String name;
+  @override
+  final String description;
+  final List<String> _ingredientName;
+  @override
+  List<String> get ingredientName {
+    if (_ingredientName is EqualUnmodifiableListView) return _ingredientName;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_ingredientName);
+  }
+
+  final List<String> _ingredientQuantity;
+  @override
+  List<String> get ingredientQuantity {
+    if (_ingredientQuantity is EqualUnmodifiableListView)
+      return _ingredientQuantity;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_ingredientQuantity);
+  }
+
+  final List<String> _stepNumber;
+  @override
+  List<String> get stepNumber {
+    if (_stepNumber is EqualUnmodifiableListView) return _stepNumber;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_stepNumber);
+  }
+
+  final List<String> _stepDescription;
+  @override
+  List<String> get stepDescription {
+    if (_stepDescription is EqualUnmodifiableListView) return _stepDescription;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_stepDescription);
+  }
+
+  @override
+  final String calorie;
+  @override
+  final String protein;
+  @override
+  final String fat;
+  @override
+  final String carbohydrate;
+
+  @override
+  String toString() {
+    return 'ImmuRecipe(id: $id, name: $name, description: $description, ingredientName: $ingredientName, ingredientQuantity: $ingredientQuantity, stepNumber: $stepNumber, stepDescription: $stepDescription, calorie: $calorie, protein: $protein, fat: $fat, carbohydrate: $carbohydrate)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_ImmuRecipe &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            const DeepCollectionEquality()
+                .equals(other._ingredientName, _ingredientName) &&
+            const DeepCollectionEquality()
+                .equals(other._ingredientQuantity, _ingredientQuantity) &&
+            const DeepCollectionEquality()
+                .equals(other._stepNumber, _stepNumber) &&
+            const DeepCollectionEquality()
+                .equals(other._stepDescription, _stepDescription) &&
+            (identical(other.calorie, calorie) || other.calorie == calorie) &&
+            (identical(other.protein, protein) || other.protein == protein) &&
+            (identical(other.fat, fat) || other.fat == fat) &&
+            (identical(other.carbohydrate, carbohydrate) ||
+                other.carbohydrate == carbohydrate));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      name,
+      description,
+      const DeepCollectionEquality().hash(_ingredientName),
+      const DeepCollectionEquality().hash(_ingredientQuantity),
+      const DeepCollectionEquality().hash(_stepNumber),
+      const DeepCollectionEquality().hash(_stepDescription),
+      calorie,
+      protein,
+      fat,
+      carbohydrate);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ImmuRecipeCopyWith<_$_ImmuRecipe> get copyWith =>
+      __$$_ImmuRecipeCopyWithImpl<_$_ImmuRecipe>(this, _$identity);
+}
+
+abstract class _ImmuRecipe implements ImmuRecipe {
+  const factory _ImmuRecipe(
+      {required final int id,
+      required final String name,
+      required final String description,
+      required final List<String> ingredientName,
+      required final List<String> ingredientQuantity,
+      required final List<String> stepNumber,
+      required final List<String> stepDescription,
+      required final String calorie,
+      required final String protein,
+      required final String fat,
+      required final String carbohydrate}) = _$_ImmuRecipe;
+
+  @override
+  int get id;
+  @override
+  String get name;
+  @override
+  String get description;
+  @override
+  List<String> get ingredientName;
+  @override
+  List<String> get ingredientQuantity;
+  @override
+  List<String> get stepNumber;
+  @override
+  List<String> get stepDescription;
+  @override
+  String get calorie;
+  @override
+  String get protein;
+  @override
+  String get fat;
+  @override
+  String get carbohydrate;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ImmuRecipeCopyWith<_$_ImmuRecipe> get copyWith =>
+      throw _privateConstructorUsedError;
+}
