0178f71ccfa596c1c130ef8a8e13d2eb37e1c608
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-13 16:33:02 +0900

    [add] ResipeServiceクラスのデータ保存とデータ全て取得のメソッドを追加

--- lib/data/sources/recipe_service.dart
+++ lib/data/sources/recipe_service.dart
@@ -1,13 +1,25 @@
-import 'package:cooking_completly_understood/data/models/message/message.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:cooking_completly_understood/data/models/recipe/recipe.dart';
+import 'package:isar/isar.dart';
 
-// class RecipeDataSource {
-//   final Isar _isar;
-//   RecipeDataSource(this._isar);
+class RecipeService {
+  final Isar _isar;
+  RecipeService(this._isar);
 
-//   Future<void> insertRecipe(Recipe recipe) async {
-//     await _isar.writeTxn(() async {
-//       await _isar.recipe.put(recipe);
-//     });
-//   }
-// }
+  // レシピを保存するメソッド
+  Future<void> insertRecipe(Recipe recipe) async {
+    await _isar.writeTxn(
+      () async {
+        await _isar.recipes.put(recipe);
+      },
+    );
+  }
+
+  // レシピを全て取得するメソッド
+  Future<List<Recipe>> getAllRecipes() async {
+    return _isar.txn(
+      () async {
+        return _isar.recipes.where().findAll();
+      },
+    );
+  }
+}
