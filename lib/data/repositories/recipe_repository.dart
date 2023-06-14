5bf3f501bc2df7e259aaf741f7e46fed1c9ba61e
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-14 19:51:57 +0900

    [update] 使用しないメソッドを削除

--- lib/data/repositories/recipe_repository.dart
+++ lib/data/repositories/recipe_repository.dart
@@ -9,30 +9,6 @@ class RecipeRepository {
   final RecipeService _recipeService;
   RecipeRepository(this._recipeService);
 
-  // レシピを保存するメソッド
-  Future<void> insertRecipeFromMessage(Message message) async {
-    //保存するレシピデータクラスを作成
-    final insertedRecipe = Recipe();
-    //レシピデータクラスに値を代入
-    insertedRecipe.name = message.recipeName;
-    insertedRecipe.description = message.recipeDescription;
-    insertedRecipe.ingredientName =
-        message.recipeIngredients.map((e) => e.name).toList();
-    insertedRecipe.ingredientQuantity =
-        message.recipeIngredients.map((e) => e.quantity).toList();
-    insertedRecipe.stepNumber =
-        message.recipeSteps.map((e) => e.number).toList();
-    insertedRecipe.stepDescription =
-        message.recipeSteps.map((e) => e.description).toList();
-    insertedRecipe.calorie = message.nutrition.calorie;
-    insertedRecipe.protein = message.nutrition.protein;
-    insertedRecipe.fat = message.nutrition.fat;
-    insertedRecipe.carbohydrate = message.nutrition.carbohydrate;
-    insertedRecipe.salt = message.nutrition.salt;
-    //レシピを保存
-    await _recipeService.insertRecipe(insertedRecipe);
-  }
-
   // レシピを全て取得するメソッド
   Stream<List<ImmuRecipe>> getAllRecipes() {
     //レシピを全て取得
