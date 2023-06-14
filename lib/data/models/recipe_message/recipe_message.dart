3b11b1cd09fddab12c9db7d7048ef8e8ddb90783
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-14 18:59:20 +0900

    [delete] REcipeMessageのデフォルトを消去

--- lib/data/models/recipe_message/recipe_message.dart
+++ lib/data/models/recipe_message/recipe_message.dart
@@ -6,14 +6,9 @@ part 'recipe_message.freezed.dart';
 @freezed
 class RecipeMessage with _$RecipeMessage {
   const factory RecipeMessage({
-    @Default(-1)
     required int id,
-    @Default('')
     required String role,
-    @Default('')
     required String content,
-    //timeStamp
-    @Default('')
     required DateTime timeStamp,
   }) = _RecipeMessage;
 }
