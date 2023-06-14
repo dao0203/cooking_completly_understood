d9be9a2d0c74bf83c3ff26870a3a5073b5ef5a91
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-14 23:08:31 +0900

    [update] コメントの消去

--- lib/ui/state/message_state.dart
+++ lib/ui/state/message_state.dart
@@ -1,14 +1,9 @@
 import 'package:cooking_completly_understood/data/models/recipe_message/recipe_message.dart';
 import 'package:cooking_completly_understood/di/message_repository_provider.dart';
-import 'package:cooking_completly_understood/di/recipe_repository_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'message_state.g.dart';
 
-//レシピは全てローカルDBに保存されるが、お勧めされたレシピが採用されたら調理済みにする
-//もっと提案ボタンを押したら、ChatGPTに負担をかけずに済むし、メッセージに表示しやすい
-//TODO:
-// - 自分のメッセージと相手のメッセージを一つのデータクラスに統一する
 @riverpod
 class MessagesState extends _$MessagesState {
   @override
@@ -20,9 +15,8 @@ class MessagesState extends _$MessagesState {
     });
   }
 
-  //TODO: メッセージを送るだけにしたい
+  //メッセージをChatGPTに送信するメソッド
   Future<void> sendMessageAndReceiveMessage(String inputedMessage) async {
-    //TODO:ChatGPTを呼び出してメッセージを返しているが、MessageRepositoryのみで完結させるようにする
     await ref.read(messageRepositoryProvider).then(
           (value) => value.sendMessage(inputedMessage),
         );
