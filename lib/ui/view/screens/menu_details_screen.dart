13325ecc34fdc91b9fa2011d348e7d7a46cd1e93
cardseditor <toshiki.u314@gmail.com>
2023-06-15 14:05:50 +0900

    feat: 料理の表示を修正

--- lib/ui/view/screens/menu_details_screen.dart
+++ lib/ui/view/screens/menu_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cooking_completly_understood/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class MenuDetailsScreen extends StatelessWidget {
@@ -6,73 +7,68 @@ class MenuDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> recipeData = {
-      "jsonRecipeName": "肉じゃが",
-      "jsonRecipeDescription": "肉じゃがは、日本の定番料理であり、ジャガイモを使った人気のあるレシピです。",
-      "jsonRecipeIngredients": [
+      jsonRecipeName: "肉じゃが",
+      jsonRecipeDescription: "肉じゃがは、日本の定番料理であり、ジャガイモを使った人気のあるレシピです。",
+      jsonRecipeIngredients: [
         {
-          "jsonIngredientName": "豚肉（または鶏肉）",
-          "jsoningredientQuantity": "300g（一口大のサイズに切る）"
+          jsonIngredientName: "豚肉（または鶏肉）",
+          jsoningredientQuantity: "300g（一口大のサイズに切る）"
         },
         {
-          "jsonIngredientName": "ジャガイモ",
-          "jsoningredientQuantity": "3〜4個（中サイズ、皮をむき一口大に切る）"
+          jsonIngredientName: "ジャガイモ",
+          jsoningredientQuantity: "3〜4個（中サイズ、皮をむき一口大に切る）"
         },
-        {"jsonIngredientName": "にんじん", "jsoningredientQuantity": "1本（一口大に切る）"},
-        {"jsonIngredientName": "玉ねぎ", "jsoningredientQuantity": "1個（みじん切り）"},
-        {"jsonIngredientName": "しょうが", "jsoningredientQuantity": "1片（みじん切り）"},
-        {"jsonIngredientName": "にんにく", "jsoningredientQuantity": "2片（みじん切り）"},
-        {"jsonIngredientName": "醤油", "jsoningredientQuantity": "3大さじ"},
-        {"jsonIngredientName": "みりん", "jsoningredientQuantity": "2大さじ"},
-        {"jsonIngredientName": "砂糖", "jsoningredientQuantity": "1大さじ"},
-        {"jsonIngredientName": "水またはだし汁", "jsoningredientQuantity": "200ml"},
-        {"jsonIngredientName": "サラダ油", "jsoningredientQuantity": "大さじ1"},
-        {"jsonIngredientName": "塩", "jsoningredientQuantity": "お好みで適量"},
-        {
-          "jsonIngredientName": "ごま油",
-          "jsoningredientQuantity": "数滴（仕上げ用、オプション）"
-        }
+        {jsonIngredientName: "にんじん", jsoningredientQuantity: "1本（一口大に切る）"},
+        {jsonIngredientName: "玉ねぎ", jsoningredientQuantity: "1個（みじん切り）"},
+        {jsonIngredientName: "しょうが", jsoningredientQuantity: "1片（みじん切り）"},
+        {jsonIngredientName: "にんにく", jsoningredientQuantity: "2片（みじん切り）"},
+        {jsonIngredientName: "醤油", jsoningredientQuantity: "3大さじ"},
+        {jsonIngredientName: "みりん", jsoningredientQuantity: "2大さじ"},
+        {jsonIngredientName: "砂糖", jsoningredientQuantity: "1大さじ"},
+        {jsonIngredientName: "水またはだし汁", jsoningredientQuantity: "200ml"},
+        {jsonIngredientName: "サラダ油", jsoningredientQuantity: "大さじ1"},
+        {jsonIngredientName: "塩", jsoningredientQuantity: "お好みで適量"},
+        {jsonIngredientName: "ごま油", jsoningredientQuantity: "数滴（仕上げ用、オプション）"}
       ],
-      "jsonRecipeSteps": [
+      jsonRecipeSteps: [
         {
-          "jsonStepNumber": "1",
-          "jsonStepDescription": "フライパンにサラダ油を熱し、しょうがとにんにくを香りが出るまで炒めます。"
+          jsonStepNumber: "1",
+          jsonStepDescription: "フライパンにサラダ油を熱し、しょうがとにんにくを香りが出るまで炒めます。"
         },
-        {"jsonStepNumber": "2", "jsonStepDescription": "豚肉を加え、色が変わるまで炒めます。"},
-        {"jsonStepNumber": "3", "jsonStepDescription": "玉ねぎを加え、透明になるまで炒めます。"},
+        {jsonStepNumber: "2", jsonStepDescription: "豚肉を加え、色が変わるまで炒めます。"},
+        {jsonStepNumber: "3", jsonStepDescription: "玉ねぎを加え、透明になるまで炒めます。"},
+        {jsonStepNumber: "4", jsonStepDescription: "醤油、みりん、砂糖を加えて調味料を絡めます。"},
         {
-          "jsonStepNumber": "4",
-          "jsonStepDescription": "醤油、みりん、砂糖を加えて調味料を絡めます。"
-        },
-        {
-          "jsonStepNumber": "5",
-          "jsonStepDescription": "ジャガイモとにんじんを加え、全体が均一に混ざるように炒めます。"
+          jsonStepNumber: "5",
+          jsonStepDescription: "ジャガイモとにんじんを加え、全体が均一に混ざるように炒めます。"
         },
         {
-          "jsonStepNumber": "6",
-          "jsonStepDescription": "水またはだし汁を加え、具材がひたひたになるくらいまで注ぎます。"
+          jsonStepNumber: "6",
+          jsonStepDescription: "水またはだし汁を加え、具材がひたひたになるくらいまで注ぎます。"
         },
         {
-          "jsonStepNumber": "7",
-          "jsonStepDescription": "弱火にして蓋をし、ジャガイモとにんじんが柔らかくなるまで煮込みます（約15〜20分）。"
+          jsonStepNumber: "7",
+          jsonStepDescription: "弱火にして蓋をし、ジャガイモとにんじんが柔らかくなるまで煮込みます（約15〜20分）。"
         },
-        {"jsonStepNumber": "8", "jsonStepDescription": "味を見て、必要に応じて塩で調整します。"},
+        {jsonStepNumber: "8", jsonStepDescription: "味を見て、必要に応じて塩で調整します。"},
         {
-          "jsonStepNumber": "9",
-          "jsonStepDescription": "ごま油を数滴たらし、仕上げに混ぜます（オプション）。"
+          jsonStepNumber: "9",
+          jsonStepDescription: "ごま油を数滴たらし、仕上げに混ぜます（オプション）。"
         },
         {
-          "jsonStepNumber": "10",
-          "jsonStepDescription":
+          jsonStepNumber: "10",
+          jsonStepDescription:
               "煮込みが終わったら、肉じゃがは完成です。ご飯やおかずと一緒にお召し上がりください。お好みで青ネギや赤唐辛子をトッピングしても美味しくいただけます。"
         }
       ],
-      "jsonRecipeNutrition": {
-        "jsonCalorie": "カロリー",
-        "jsonProtein": "タンパク質",
-        "jsonFat": "脂質",
-        "jsonCarbohydrate": "炭水化物"
+      jsonRecipeNutrition: {
+        jsonCalorie: "カロリー",
+        jsonProtein: "タンパク質",
+        jsonFat: "脂質",
+        jsonCarbohydrate: "炭水化物"
       }
     };
+
     return Scaffold(
       appBar: AppBar(title: const Text('肉じゃが完全理解')),
       body: SingleChildScrollView(
@@ -110,9 +106,9 @@ class MenuDetailsScreen extends StatelessWidget {
                     style:
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 const SizedBox(height: 10),
-                ...recipeData["jsonRecipeIngredients"].map((ingredient) {
+                ...recipeData[jsonRecipeIngredients].map((ingredient) {
                   return Text(
-                    '${ingredient["jsonIngredientName"]}: ${ingredient["jsoningredientQuantity"]}',
+                    '${ingredient[jsonIngredientName]}: ${ingredient[jsoningredientQuantity]}',
                     style: const TextStyle(fontSize: 16),
                   );
                 }).toList(),
@@ -121,15 +117,13 @@ class MenuDetailsScreen extends StatelessWidget {
                     style:
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 const SizedBox(height: 10),
-                ...recipeData["jsonRecipeSteps"].map((step) {
+                ...recipeData[jsonRecipeSteps].map((step) {
                   return ListTile(
-                    leading: Text(step["jsonStepNumber"],
+                    leading: Text(step[jsonStepNumber],
                         style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )),
+                            fontSize: 18, fontWeight: FontWeight.bold)),
                     title: Text(
-                      step["jsonStepDescription"],
+                      step[jsonStepDescription],
                       style: const TextStyle(fontSize: 16),
                     ),
                   );
