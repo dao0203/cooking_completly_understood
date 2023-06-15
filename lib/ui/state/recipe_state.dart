baf649fc88833172c14f04579d26318f280862a9
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-15 23:53:42 +0900

    [add] Recipeの状態管理を追加

--- lib/ui/state/recipe_state.dart
+++ lib/ui/state/recipe_state.dart
@@ -2,14 +2,14 @@ import 'package:cooking_completly_understood/data/models/immu_recipe/immu_recipe
 import 'package:cooking_completly_understood/di/recipe_repository_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-// part 'recipe_state.g.dart';
+part 'recipe_state.g.dart';
 
-// @riverpod
-// class RecipeState extends _$RecipeState {
-//   @override
-//   Future<List<ImmuRecipe>> build() {
-//     return ref.watch(recipeRepositoryProvider).then(
-//           (value) => value.getAllRecipes(),
-//         );
-//   }
-// }
+@riverpod
+class RecipeState extends _$RecipeState {
+  @override
+  Future<Stream<List<ImmuRecipe>>> build() {
+    return ref.read(recipeRepositoryProvider).then(
+          (value) => value.getAllRecipes(),
+        );
+  }
+}
