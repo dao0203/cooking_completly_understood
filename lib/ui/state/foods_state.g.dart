6d93042faf5e1acb81f37ffea5dbd8ee6586e591
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-17 00:23:24 +0900

    [add] 追加するの忘れてた

---
+++ lib/ui/state/foods_state.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'foods_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$foodsStateHash() => r'766988134e761dd448924e8864104f6447466538';
+
+/// See also [FoodsState].
+@ProviderFor(FoodsState)
+final foodsStateProvider = AutoDisposeAsyncNotifierProvider<FoodsState,
+    Stream<List<ImmuFood>>>.internal(
+  FoodsState.new,
+  name: r'foodsStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$foodsStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FoodsState = AutoDisposeAsyncNotifier<Stream<List<ImmuFood>>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
