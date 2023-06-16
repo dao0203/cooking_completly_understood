a2842cfb41bcc1c2129e2c86f6d93b897fb6407e
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-17 08:42:26 +0900

    [add] PalMAPI の単体テスト

---
+++ test/units/maker_suite_service/get_message_is_not_null_test.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:cooking_completly_understood/data/sources/maker_suite_service.dart';
+import 'package:cooking_completly_understood/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_mlkit_translation/google_mlkit_translation.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  late MakerSuiteService makerSuiteService;
+  late String sendedBodyString;
+  late String promptString;
+  setUpAll(() async {
+    //「大根を使ったレシピを教えて」を英語で
+    promptString = "please give me a recipe that uses daikon";
+
+    //MakerSuiteServiceを作成
+    makerSuiteService = MakerSuiteService.create();
+
+    final sendedMessage = messageThatUserInputtedInEnglish(promptString, '30', '3');
+    //送信するリクエストボディを作成
+    sendedBodyString = json.encode(getRequestBodyForMakerSuite(sendedMessage));
+  });
+
+  //テストケースごとに実行される
+  test('get message is not null', () async {
+    final response = await makerSuiteService.getMessage(sendedBodyString);
+    debugPrint(response.toString());
+    expect(response, isNotNull);
+  });
+}
