16d28b47a45ed7f723c1a3a051017dee083cfbcd
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-17 00:14:32 +0900

    [add] FoodServiceの依存性注入をProviderで注入

---
+++ lib/di/food_service_provider.dart
@@ -0,0 +1,9 @@
+import 'package:cooking_completly_understood/data/sources/food_service.dart';
+import 'package:cooking_completly_understood/di/isar_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final foodServiceProvider = Provider((ref) {
+  return ref.read(isarProvider).then(
+        (isar) => FoodService(isar),
+      );
+});
