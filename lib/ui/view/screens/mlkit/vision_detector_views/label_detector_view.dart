a9e033ebab5d825725dece830a5bc4b713eaeab8
cardseditor <toshiki.u314@gmail.com>
2023-06-17 05:31:03 +0900

    feat: カメラによる食材追加

--- lib/ui/view/screens/mlkit/vision_detector_views/label_detector_view.dart
+++ lib/ui/view/screens/mlkit/vision_detector_views/label_detector_view.dart
@@ -1,22 +1,24 @@
 import 'dart:io' as io;
 
+import 'package:cooking_completly_understood/ui/state/foods_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'camera_view.dart';
 import 'painters/label_detector_painter.dart';
 
-class ImageLabelView extends StatefulWidget {
+class ImageLabelView extends ConsumerStatefulWidget {
   const ImageLabelView({super.key});
 
   @override
-  State<ImageLabelView> createState() => _ImageLabelViewState();
+  ConsumerState<ImageLabelView> createState() => _ImageLabelViewState();
 }
 
-class _ImageLabelViewState extends State<ImageLabelView> {
+class _ImageLabelViewState extends ConsumerState<ImageLabelView> {
   late ImageLabeler _imageLabeler;
   bool _canProcess = false;
   bool _isBusy = false;
@@ -77,6 +79,8 @@ class _ImageLabelViewState extends State<ImageLabelView> {
   }
 
   Future<void> processImage(InputImage inputImage) async {
+    final foodsStateNotifier = ref.watch(foodsStateProvider.notifier);
+
     if (!_canProcess) return;
     if (_isBusy) return;
     _isBusy = true;
@@ -86,7 +90,7 @@ class _ImageLabelViewState extends State<ImageLabelView> {
     final labels = await _imageLabeler.processImage(inputImage);
     if (inputImage.metadata?.size != null &&
         inputImage.metadata?.rotation != null) {
-      final painter = LabelDetectorPainter(labels);
+      final painter = LabelDetectorPainter(labels, foodsStateNotifier);
       _customPaint = CustomPaint(painter: painter);
     } else {
       String text = 'Labels found: ${labels.length}\n\n';
