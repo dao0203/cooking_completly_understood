120b3fb126905c00fd044545de74676830174bca
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-16 23:28:18 +0900

    [add] 調理済みかお気に入りかで変わるアイコンを表示。

--- lib/ui/view/widget/bottom_sheet_recipe.dart
+++ lib/ui/view/widget/bottom_sheet_recipe.dart
@@ -25,46 +25,106 @@ class BottomSheetRecipe extends HookConsumerWidget {
                 );
               } else {
                 final recipe = snapshot.data!;
-                return ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 1, //レシピは1つしかないので1
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        
-                        //レシピ名
-                        Text(
-                          recipe[index].name,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                return SizedBox(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 1, //レシピは1つしかないので1
+                    itemBuilder: (context, index) {
+                      //TODO: これを使用してデータを表示お願いします！
+                      final recipeContent = recipe[index];
+                      return Column(
+                        children: [
+                          //チェックのアイコンを表示
+                          IconButton(
+                            icon: const Icon(Icons.check),
+                            onPressed: () {
+                              ref.read(recipeByIdStateProvider(recipeId).notifier).changeMadeStatus(recipeContent);
+                            },
+                            color: recipeContent.isMade
+                                ? Colors.green
+                                : Colors.grey,
                           ),
-                        ),
-                        //レシピの説明
-                        Text(
-                          recipe[index].description,
-                          style: const TextStyle(
-                            fontSize: 15,
+
+                          //星のアイコンを表示
+                          IconButton(
+                            icon: const Icon(Icons.star),
+                            onPressed: () {
+                              ref.read(recipeByIdStateProvider(recipeId).notifier).changeFavoriteStatus(recipeContent);
+                            },
+                            color: recipeContent.isFavorite
+                                ? Colors.yellow
+                                : Colors.grey,
                           ),
-                        ),
-                        //レシピの材料
-                        Text(
-                          recipe[index].ingredientName.toString(),
-                          style: const TextStyle(
-                            fontSize: 15,
+
+                          //レシピ名
+                          Text(
+                            recipe[index].name,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        //レシピの手順
-                        Text(
-                          recipe[index].stepDescription.toString(),
-                          style: const TextStyle(
-                            fontSize: 15,
+                          //レシピの説明
+                          Text(
+                            recipe[index].description,
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  },
+                          //レシピの材料
+                          Text(
+                            recipe[index].ingredientName.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          //レシピの手順
+                          Text(
+                            recipe[index].stepDescription.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          //レシピのカロリー
+                          Text(
+                            recipe[index].calorie.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          //レシピのタンパク質
+                          Text(
+                            recipe[index].protein.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          //レシピの脂質
+                          Text(
+                            recipe[index].fat.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          //レシピの炭水化物
+                          Text(
+                            recipe[index].carbohydrate.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          //レシピの塩分
+                          Text(
+                            recipe[index].salt.toString(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                 );
               }
             },
