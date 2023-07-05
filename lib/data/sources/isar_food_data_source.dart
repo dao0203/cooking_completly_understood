931f6b05ba1a8626b5db0bff74e014f16d418df2
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-05 23:32:19 +0900

    [add] FoodDataSourceを継承したIsarDataSourceを追加

--- lib/data/sources/isar_food_data_source.dart
+++ lib/data/sources/isar_food_data_source.dart
@@ -1,19 +1,22 @@
 import 'package:cooking_completly_understood/data/models/food/food.dart';
+import 'package:cooking_completly_understood/data/sources/food_data_source.dart';
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 
-class FoodService {
+class IsarFoodDataSource implements FoodDataSource {
   final Isar _isar;
-  FoodService(this._isar);
+  IsarFoodDataSource(this._isar);
 
   Query<Food> get _foodGetAllQuery => _isar.foods.where(sort: Sort.asc).build();
 
   //食べ物を全て取得するメソッド
+  @override
   Stream<List<Food>> getAllFood() {
     return _foodGetAllQuery.watch(fireImmediately: true);
   }
-  
+
   //食べ物を保存するメソッド
+  @override
   Future<void> insertFood(Food food) async {
     await _isar.writeTxn(
       () async {
@@ -23,10 +26,13 @@ class FoodService {
   }
 
   //食べ物を削除するメソッド
+  @override
   Future<void> deleteFood(int foodId) async {
     await _isar.writeTxn(
       () async {
-        await _isar.foods.delete(foodId).then((value) => debugPrint("食べ物を削除しました"));
+        await _isar.foods
+            .delete(foodId)
+            .then((value) => debugPrint("食べ物を削除しました"));
       },
     );
   }
