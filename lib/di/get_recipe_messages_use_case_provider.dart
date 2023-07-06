2f0467426d78dd68af4cc305ba3459ee0344b48a
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-07 07:42:01 +0900

    [update] UseCaseをuse_casesフォルダに移動

--- lib/di/get_recipe_messages_use_case_provider.dart
+++ lib/di/get_recipe_messages_use_case_provider.dart
@@ -2,7 +2,7 @@
 
 import 'package:cooking_completly_understood/di/message_repository_provider.dart';
 import 'package:cooking_completly_understood/di/recipe_repository_provider.dart';
-import 'package:cooking_completly_understood/domain/get_recipe_messages_use_case.dart';
+import 'package:cooking_completly_understood/use_cases/get_recipe_messages_use_case.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final getRecipeMessagesUseCaseProvider = Provider((ref) async{
