22dbfbf456801b1a9513b16fabad6bba407482c6
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-08 05:00:48 +0900

    [add] UseCaseのProviderを一つのファイルで一元管理

--- lib/di/get_recipe_messages_use_case_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cooking_completly_understood/di/repository_providers.dart';
-import 'package:cooking_completly_understood/domain/use_cases/get_recipe_messages_use_case.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final getRecipeMessagesUseCaseProvider = Provider((ref) async {
-  final messageRepository = await ref.read(messageRepositoryProvider);
-  final recipeRepository = await ref.read(recipeRepositoryProvider);
-  return GetRecipeMessagesUseCase(messageRepository, recipeRepository);
-});
---
+++ lib/di/use_case_providers.dart
@@ -0,0 +1,26 @@
+import 'package:cooking_completly_understood/di/repository_providers.dart';
+import 'package:cooking_completly_understood/domain/use_cases/get_recipe_messages_use_case.dart';
+import 'package:cooking_completly_understood/domain/use_cases/suggest_recipe_considering_weather_and_temperature_use_case.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final getRecipeMessagesUseCaseProvider = Provider((ref) async {
+  final myMessageRepository = await ref.read(myMessageRepositoryProvider);
+  final recipeRepository = await ref.read(recipeRepositoryProvider);
+  return GetRecipeMessagesUseCase(myMessageRepository, recipeRepository);
+});
+
+final suggestRecipeConsideringWeatherAndTEmperatureUseCaseProvider =
+    Provider((ref) async {
+  final myMessageRepository = await ref.read(myMessageRepositoryProvider);
+  final positionRepository = ref.read(positionRepositoryProvider);
+  final weatherRepository = ref.read(weatherRepositoryProvider);
+  final messageRepository = ref.read(messageRepositoryProvider);
+  final recipeRepository = await ref.read(recipeRepositoryProvider);
+  return  SuggestRecipeConsideringWeatherAndTemperatureUseCase(
+    myMessageRepository,
+    positionRepository,
+    weatherRepository,
+    messageRepository,
+    recipeRepository,
+  );
+});
