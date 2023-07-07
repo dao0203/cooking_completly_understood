4f2c0dd511078899f49cff480d8ed26d8c36f10a
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-08 05:15:32 +0900

    [add] UseCaseを実装

--- lib/domain/use_cases/get_recipe_messages_use_case.dart
+++ lib/domain/use_cases/get_recipe_messages_use_case.dart
@@ -1,20 +1,22 @@
 import 'dart:async';
 
-import 'package:cooking_completly_understood/data/models/my_message_model/my_message_model.dart';
-import 'package:cooking_completly_understood/data/models/recipe_model/recipe_model.dart';
+import 'package:cooking_completly_understood/domain/models/my_message/my_message.dart';
+import 'package:cooking_completly_understood/domain/models/recipe/recipe.dart';
 import 'package:cooking_completly_understood/domain/models/recipe_message/recipe_message.dart';
-import 'package:cooking_completly_understood/data/repositories/message_repository.dart';
-import 'package:cooking_completly_understood/data/repositories/recipe_repository.dart';
+import 'package:cooking_completly_understood/domain/repositories/my_message_repository.dart';
+import 'package:cooking_completly_understood/domain/repositories/recipe_repository.dart';
+import 'package:cooking_completly_understood/domain/use_cases/use_case.dart';
 import 'package:rxdart/rxdart.dart';
 
-class GetRecipeMessagesUseCase {
-  final MessageRepository _messageRepository;
+class GetRecipeMessagesUseCase
+    implements UseCase<void, Stream<List<RecipeMessage>>> {
+  final MyMessageRepository _messageRepository;
   final RecipeRepository _recipeRepository;
 
   GetRecipeMessagesUseCase(this._messageRepository, this._recipeRepository);
   //RecipeからRecipeMessageへ変換するトランスフォーマー
   final recipeToRecipeMessageTransformer =
-      StreamTransformer<List<RecipeModel>, List<RecipeMessage>>.fromHandlers(
+      StreamTransformer<List<Recipe>, List<RecipeMessage>>.fromHandlers(
     handleData: (recipes, sink) {
       final recipeMessages = recipes
           .map(
@@ -32,7 +34,7 @@ class GetRecipeMessagesUseCase {
 
 //MyMessageからRecipeMessageへ変換するトランスフォーマー
   final myMessageToRecipeMessageTransformer =
-      StreamTransformer<List<MyMessageModel>, List<RecipeMessage>>.fromHandlers(
+      StreamTransformer<List<MyMessage>, List<RecipeMessage>>.fromHandlers(
     handleData: (myMessages, sink) {
       final recipeMessages = myMessages
           .map(
@@ -49,11 +51,12 @@ class GetRecipeMessagesUseCase {
   );
 
 //RecipeとMyMessageを結合するメソッド
-  Stream<List<RecipeMessage>> call() {
+  @override
+  Stream<List<RecipeMessage>> call(void arg) {
     final recipeStream = _recipeRepository.getAllRecipes().transform(
           recipeToRecipeMessageTransformer,
         );
-    final myMessageStream = _messageRepository.getAllMyMessages().transform(
+    final myMessageStream = _messageRepository.getAll().transform(
           myMessageToRecipeMessageTransformer,
         );
     return Rx.combineLatest2(
