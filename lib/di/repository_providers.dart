3374354695931e61fc962d939fa49959a6ffe0f0
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-08 05:16:27 +0900

    [add]すべてのRepositoryのProviderを追加

--- lib/di/repository_providers.dart
+++ lib/di/repository_providers.dart
@@ -1,29 +1,36 @@
 //RecipeRepository
 import 'package:cooking_completly_understood/data/repositories/food_repository_impl.dart';
-import 'package:cooking_completly_understood/data/repositories/message_repository.dart';
-import 'package:cooking_completly_understood/data/repositories/recipe_repository.dart';
+import 'package:cooking_completly_understood/data/repositories/message_repository_impl.dart';
+import 'package:cooking_completly_understood/data/repositories/my_message_repository_impl.dart';
+import 'package:cooking_completly_understood/data/repositories/position_repository_impl.dart';
+import 'package:cooking_completly_understood/data/repositories/recipe_repository_impl.dart';
+import 'package:cooking_completly_understood/data/repositories/weather_repository_impl.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'data_source_providers.dart';
 
 //RecipeRepository
 final recipeRepositoryProvider = Provider((ref) => ref
     .read(recipeDataSourceProvider)
-    .then((value) => RecipeRepository(value)));
+    .then((value) => RecipeRepositoryImpl(value)));
 
 //MessageRepository
-final messageRepositoryProvider = Provider((ref) => ref
-    .read(recipeDataSourceProvider)
-    .then((recipeService) => ref
-        .read(myMessageDataSourceProvider)
-        .then((myMessage) => MessageRepository(
-              ref.read(positionDataSourceProvider),
-              ref.read(openMeteoApiDataSourceProvider),
-              ref.read(paLMApiDataSourceProvider),
-              recipeService,
-              myMessage,
-            ))));
+final messageRepositoryProvider = Provider(
+    (ref) => MessageRepositoryImpl(ref.read(paLMApiDataSourceProvider)));
 
 //FoodRepository
 final foodRepositoryProvider = Provider((ref) => ref
     .read(foodDataSourceProvider)
     .then((value) => FoodRepositoryImpl(value)));
+
+//MyMessageRepository
+final myMessageRepositoryProvider = Provider((ref) => ref
+    .read(myMessageDataSourceProvider)
+    .then((value) => MyMessageRepositoryImpl(value)));
+
+//PositionRepository
+final positionRepositoryProvider = Provider(
+    (ref) => PositionRepositoryImpl(ref.read(positionDataSourceProvider)));
+
+//WeatherRepository
+final weatherRepositoryProvider = Provider(
+    (ref) => WeatherRepositoryImpl(ref.read(weatherDataSourceProvider)));
