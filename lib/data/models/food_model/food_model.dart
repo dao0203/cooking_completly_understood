8c6b288e5a1b6fef4d5df51d79ea16c052dc3cc1
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-07 13:20:16 +0900

    [add] FoodModelからFoodに変換するメソッドを追加

--- lib/data/models/food_model/food_model.dart
+++ lib/data/models/food_model/food_model.dart
@@ -1,3 +1,4 @@
+import 'package:cooking_completly_understood/domain/models/food/food.dart';
 import 'package:isar/isar.dart';
 
 part 'food_model.g.dart';
@@ -7,4 +8,12 @@ class FoodModel {
   Id id = Isar.autoIncrement;
   late String name;
   late DateTime createdAt;
+
+  Food toFood() {
+    return Food(
+      id: id,
+      name: name,
+      createdAt: createdAt,
+    );
+  }
 }
