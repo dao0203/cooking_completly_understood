64962a972bf88585d5d90c94ced1dfd6eebdb09d
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-07 22:38:50 +0900

    [add] PaLMApiDataSourceのgetMessageの単体テストを実装

--- test/units/palm_api_data_source/get_message_is_not_null_test.dart
+++ test/units/palm_api_data_source/get_message_is_not_null_test.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 
-import 'package:cooking_completly_understood/data/services/maker_suite_service.dart';
+import 'package:cooking_completly_understood/data/sources/remote/palm_api_data_source.dart';
 import 'package:cooking_completly_understood/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  late MakerSuiteService makerSuiteService;
+  late PaLMApiDataSource paLMApiDataSource;
   late String sendedBodyString;
   late String promptString;
   setUpAll(() async {
@@ -15,16 +15,17 @@ void main() {
     promptString = "please give me a recipe that uses daikon";
 
     //MakerSuiteServiceを作成
-    makerSuiteService = MakerSuiteService.create();
+    paLMApiDataSource = PaLMApiDataSource.create();
 
-    final sendedMessage = messageThatUserInputtedInEnglish(promptString, '30', '3');
+    final sendedMessage =
+        messageThatUserInputtedInEnglish(promptString, '30', '3');
     //送信するリクエストボディを作成
-    sendedBodyString = json.encode(getRequestBodyForMakerSuite(sendedMessage));
+    sendedBodyString = json.encode(getRequestBodyForPaLMApi(sendedMessage));
   });
 
   //テストケースごとに実行される
   test('get message is not null', () async {
-    final response = await makerSuiteService.getMessage(sendedBodyString);
+    final response = await paLMApiDataSource.getMessage(sendedBodyString);
     debugPrint(response.toString());
     expect(response, isNotNull);
   });
