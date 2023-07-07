c42961e024f75c49f33400219aca5ab90012498d
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-07 12:57:16 +0900

    [add] ドメインモデルに天気データクラスを実装

---
+++ lib/domain/models/weather/weather.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'weather.freezed.dart';
+
+@freezed
+class Weather with _$Weather {
+  const factory Weather({
+    required double temperature,
+    required int weatherCode,
+  }) = _Weather;
+}
