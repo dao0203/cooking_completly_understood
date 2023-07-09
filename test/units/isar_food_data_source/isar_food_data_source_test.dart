06ee09fd2595e25b2116973b3380e62f1240e9ac
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-09 22:18:39 +0900

    [update] IsarFoodDataSourceのテスト環境の整備

--- test/units/isar_food_data_source/isar_food_data_source_test.dart
+++ test/units/isar_food_data_source/isar_food_data_source_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:cooking_completly_understood/data/models/food_model/food_model.dart';
 import 'package:cooking_completly_understood/data/sources/local/isar_food_data_source.dart';
 import 'package:flutter/material.dart';
@@ -8,15 +7,16 @@ import 'package:isar/isar.dart';
 import '../../mocks/mock_food_models.dart';
 import '../../utils/isar_db_directory.dart';
 
-void main(){
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   late Isar isar;
   late IsarFoodDataSource isarFoodDataSource;
   late List<FoodModel> getAllResult;
   late List<FoodModel> insertResult;
+  late List<FoodModel> deleteResult;
   final IsarDbDirectory isarDbDirectory = IsarDbDirectory();
 
-  setUpAll(()async{
+  setUpAll(() async {
     await Isar.initializeIsarCore(download: true);
     await isarDbDirectory.create();
 
@@ -24,82 +24,73 @@ void main(){
 
     isarFoodDataSource = IsarFoodDataSource(isar);
 
-    for(final foodModel in mockFoodModelList){
+    for (final foodModel in mockFoodModelList) {
       await isarFoodDataSource.insertFood(foodModel);
     }
-
-    getAllResult = await isarFoodDataSource.getAllFood().first;
-
   });
 
-  tearDownAll(()async{
+  tearDownAll(() async {
     await isar.close();
     await isarDbDirectory.delete();
   });
 
-  group('getAllFood', (){
-    test('getAllFoodの結果の長さがmockFoodModelListの長さと同じであること', (){
+  group('getAllFood', () {
+    setUpAll(() async {
+      getAllResult = await isarFoodDataSource.getAllFood().first;
+    });
+    test('getAllFoodの結果の長さがmockFoodModelListの長さと同じであること', () {
       expect(getAllResult.length, mockFoodModelList.length);
     });
 
-    test('getAllFoodの結果のnameがmockFoodModelListのnameと同じであること', (){
-      for(int i = 0; i < getAllResult.length; i++){
+    test('getAllFoodの結果のnameがmockFoodModelListのnameと同じであること', () {
+      for (int i = 0; i < getAllResult.length; i++) {
         expect(getAllResult[i].name, mockFoodModelList[i].name);
       }
     });
 
-    test('getAllFoodの結果のcreatedAtがmockFoodModelListのcreatedAtと同じであること', (){
-      for(int i = 0; i < getAllResult.length; i++){
+    test('getAllFoodの結果のcreatedAtがmockFoodModelListのcreatedAtと同じであること', () {
+      for (int i = 0; i < getAllResult.length; i++) {
         expect(getAllResult[i].createdAt, mockFoodModelList[i].createdAt);
       }
     });
   });
 
-  group('insertFood', (){
-    test('insertFoodの結果の長さがgetAllFoodの結果の長さより1大きいこと', ()async{
-      final foodModel = FoodModel()
-        ..name = 'test4'
-        ..createdAt = DateTime.now();
-      await isarFoodDataSource.insertFood(foodModel);
+  group('insertFood', () {
+    setUpAll(() async {
+      await isarFoodDataSource.insertFood(mockFoodModelForInsertion);
       insertResult = await isarFoodDataSource.getAllFood().first;
+    });
+
+    test('insertFoodの結果の長さがmockFoodModelListの長さより1大きいこと', () async {
       expect(insertResult.length, getAllResult.length + 1);
     });
 
-    test('insertFoodの結果のnameがgetAllFoodの結果のnameと同じであること', ()async{
-      final foodModel = FoodModel()
-        ..name = 'test4'
-        ..createdAt = DateTime.now();
-      await isarFoodDataSource.insertFood(foodModel);
-      insertResult = await isarFoodDataSource.getAllFood().first;
-      expect(insertResult.last.name, foodModel.name);
+    test('insertFoodの結果のnameがmockFoodModelListのnameと同じであること', () async {
+      expect(insertResult.last.name, mockFoodModelForInsertion.name);
     });
 
-    test('insertFoodの結果のcreatedAtがgetAllFoodの結果のcreatedAtと同じであること', ()async{
-      final foodModel = FoodModel()
-        ..name = 'test4'
-        ..createdAt = DateTime.now();
-      await isarFoodDataSource.insertFood(foodModel);
-      insertResult = await isarFoodDataSource.getAllFood().first;
-      expect(insertResult.last.createdAt, foodModel.createdAt);
+    test('insertFoodの結果のcreatedAtがmockFoodModelListのcreatedAtと同じであること',
+        () async {
+      expect(insertResult.last.createdAt, mockFoodModelForInsertion.createdAt);
     });
   });
 
-  group('deleteFood', (){
-    test('deleteFoodの結果の長さがgetAllFoodの結果の長さより1小さいこと', ()async{
+  group('deleteFood', () {
+    setUpAll(() async {
+      getAllResult = await isarFoodDataSource.getAllFood().first;
       await isarFoodDataSource.deleteFood(getAllResult.last.id);
-      final deleteResult = await isarFoodDataSource.getAllFood().first;
+      deleteResult = await isarFoodDataSource.getAllFood().first;
+    });
+
+    test('deleteFoodの結果の長さがgetAllFoodの結果の長さより1小さいこと', () async {
       expect(deleteResult.length, getAllResult.length - 1);
     });
 
-    test('deleteFoodの結果のnameがgetAllFoodの結果のnameと同じでないこと', ()async{
-      await isarFoodDataSource.deleteFood(getAllResult.last.id);
-      final deleteResult = await isarFoodDataSource.getAllFood().first;
+    test('deleteFoodの結果のnameがgetAllFoodの結果のnameと同じでないこと', () async {
       expect(deleteResult.last.name, isNot(getAllResult.last.name));
     });
 
-    test('deleteFoodの結果のcreatedAtがgetAllFoodの結果のcreatedAtと同じでないこと', ()async{
-      await isarFoodDataSource.deleteFood(getAllResult.last.id);
-      final deleteResult = await isarFoodDataSource.getAllFood().first;
+    test('deleteFoodの結果のcreatedAtがgetAllFoodの結果のcreatedAtと同じでないこと', () async {
       expect(deleteResult.last.createdAt, isNot(getAllResult.last.createdAt));
     });
   });
