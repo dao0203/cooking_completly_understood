fbf8303c2bce2cd95b27e1a79e0ff09f4be92e54
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-09 20:55:03 +0900

    [add] FoodModelListのMockを追加

---
+++ test/mocks/mock_food_model_list.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:cooking_completly_understood/data/models/food_model/food_model.dart';
+
+final List<FoodModel> mockFoodModelList = [
+  FoodModel()
+    ..name = 'test1'
+    ..createdAt = DateTime.now(),
+  FoodModel()
+    ..name = 'test2'
+    ..createdAt = DateTime.now(),
+  FoodModel()
+    ..name = 'test3'
+    ..createdAt = DateTime.now(),
+];
