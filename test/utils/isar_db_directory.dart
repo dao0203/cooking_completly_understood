9ff05efdfe53e565c64b20aaaee5e3bf9ec4c311
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-09 18:14:51 +0900

    [add] 消去するファイルを指定

--- test/utils/isar_db_directory.dart
+++ test/utils/isar_db_directory.dart
@@ -11,5 +11,7 @@ class IsarDbDirectory {
   Future<void> delete() async {
     final directory = Directory(path);
     await directory.delete(recursive: true);
+    //Isar.initializeIsarCore(download: true);を実行したときに生成されるファイルを削除するためのコード
+    await File("libisar.dylib").delete();
   }
 }
