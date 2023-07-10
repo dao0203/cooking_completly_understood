16ef5ff3e53de14bdb1eafc560fe340888d51530
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-11 01:21:33 +0900

    [add] 一番最初にメッセージを投げるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
 import 'package:cooking_completly_understood/ui/theme/app_theme.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -10,7 +11,16 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // .evnから環境変数を読み込む
   await dotenv.load(fileName: '.env');
-
+  OpenAI.apiKey = dotenv.env['OPEN_AI_API_KEY']!;
+  await OpenAI.instance.chat.create(
+    model: 'gpt-3.5-turbo',
+    messages: [
+      const OpenAIChatCompletionChoiceMessageModel(
+        role: OpenAIChatMessageRole.system,
+        content: 'あなたは、家庭料理のプロフェッショナルです',
+      ),
+    ],
+  );
   cameras = await availableCameras();
 
   runApp(
