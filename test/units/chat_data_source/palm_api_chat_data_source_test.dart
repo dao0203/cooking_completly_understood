8322b58c9589ceb8744d083060f79a375c86b102
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-10 22:59:48 +0900

    [update] PaLM APIDataSourceの英語と日本語の時のテストケースを追加

--- test/units/chat_data_source/palm_api_chat_data_source_test.dart
+++ test/units/chat_data_source/palm_api_chat_data_source_test.dart
@@ -23,35 +23,95 @@ void main() {
     paLMKey = dotenv.env['PALM_KEY']!;
   });
 
-  group("レシピをメッセージとして送信したときのテスト", () {
-    group("大根を使ったレシピを教えてと送ったときのテスト", () {
-      setUpAll(() async {
-        //「大根を使ったレシピを教えて」を英語で
-        promptString = "please give me a recipe that uses daikon";
-        final sendedMessage =
-            messageThatUserInputtedInEnglish(promptString, '30', '3');
-        //送信するリクエストボディを作成
-        sendedBodyString = json.encode(getRequestBodyForPaLMApi(sendedMessage));
-
-        //レスポンスを取得
-        response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
-      });
-      test('取得したメッセージが空ではないこと', () async {
-        expect(response, isNotNull);
-      });
-
-      test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
-        expect(response.bodyString, contains('recipe_name'));
-      });
-
-      test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
-        expect(response.bodyString, contains('recipe_description'));
-      });
-
-      group("ポテトを使ったレシピを教えてと送ったときのテスト", () {
+  group("英語でのテスト", () {
+    group("レシピをメッセージとして送信したときのテスト", () {
+      group("大根を使ったレシピを教えてと送ったときのテスト", () {
         setUpAll(() async {
-          //「ポテトを使ったレシピを教えて」を英語で
-          promptString = "please give me a recipe that uses potato";
+          //「大根を使ったレシピを教えて」を英語で
+          promptString = "please give me a recipe that uses daikon";
+          final sendedMessage =
+              messageThatUserInputtedInEnglish(promptString, '30', '3');
+          //送信するリクエストボディを作成
+          sendedBodyString =
+              json.encode(getRequestBodyForPaLMApi(sendedMessage));
+
+          //レスポンスを取得
+          response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+          debugPrint(response.bodyString);
+        });
+        test('取得したメッセージが空ではないこと', () async {
+          expect(response, isNotNull);
+        });
+
+        test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_name'));
+        });
+
+        test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_description'));
+        });
+
+        group("ポテトを使ったレシピを教えてと送ったときのテスト", () {
+          setUpAll(() async {
+            //「ポテトを使ったレシピを教えて」を英語で
+            promptString = "please give me a recipe that uses potato";
+            final sendedMessage =
+                messageThatUserInputtedInEnglish(promptString, '30', '3');
+            //送信するリクエストボディを作成
+            sendedBodyString =
+                json.encode(getRequestBodyForPaLMApi(sendedMessage));
+
+            //レスポンスを取得
+            response =
+                await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+          });
+          test('取得したメッセージが空ではないこと', () async {
+            expect(response, isNotNull);
+          });
+
+          test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
+            expect(response.bodyString, contains('recipe_name'));
+          });
+
+          test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
+            expect(response.bodyString, contains('recipe_description'));
+          });
+        });
+      });
+    });
+
+    group("レシピ以外をメッセージとして送信したときのテスト", () {
+      group("あなたの友達は何人かとを送信したときのテスト", () {
+        setUpAll(() async {
+          // 「友達は何人か」を英語で
+          promptString = "how many your friends do you have";
+          final sendedMessage =
+              messageThatUserInputtedInEnglish(promptString, '30', '3');
+          //送信するリクエストボディを作成
+          sendedBodyString =
+              json.encode(getRequestBodyForPaLMApi(sendedMessage));
+
+          //レスポンスを取得
+          response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+          debugPrint(response.bodyString);
+        });
+        test('取得したメッセージが空ではないこと', () async {
+          expect(response, isNotNull);
+        });
+
+        test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_name'));
+        });
+
+        test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_description'));
+        });
+      });
+
+      group("世界人口を教えてと送信したときのテスト", () {
+        setUpAll(() async {
+          // 「世界人口を教えて」を英語で
+          promptString = "please tell me the world population";
           final sendedMessage =
               messageThatUserInputtedInEnglish(promptString, '30', '3');
           //送信するリクエストボディを作成
@@ -76,54 +136,104 @@ void main() {
     });
   });
 
-  group("レシピ以外をメッセージとして送信したときのテスト", () {
-    group("あなたの友達は何人かとを送信したときのテスト", () {
-      setUpAll(() async {
-        // 「友達は何人か」を英語で
-        promptString = "how many your friends do you have";
-        final sendedMessage =
-            messageThatUserInputtedInEnglish(promptString, '30', '3');
-        //送信するリクエストボディを作成
-        sendedBodyString = json.encode(getRequestBodyForPaLMApi(sendedMessage));
+  group("日本語でのテスト", () {
+    group("レシピをメッセージとして送信したときのテスト", () {
+      group("大根を使ったレシピを教えてと送ったときのテスト", () {
+        setUpAll(() async {
+          //「大根を使ったレシピを教えて」を日本語で
+          promptString = "大根を使ったレシピを教えて";
+          final sendedMessage =
+              messageThatUserInputted(promptString, '30', '3');
+          //送信するリクエストボディを作成
+          sendedBodyString =
+              json.encode(getRequestBodyForPaLMApi(sendedMessage));
 
-        //レスポンスを取得
-        response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
-      });
-      test('取得したメッセージが空ではないこと', () async {
-        expect(response, isNotNull);
+          //レスポンスを取得
+          response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+        });
+        test('取得したメッセージが空ではないこと', () async {
+          expect(response, isNotNull);
+        });
+
+        test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_name'));
+        });
+
+        test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_description'));
+        });
       });
 
-      test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
-        expect(response.bodyString, contains('recipe_name'));
-      });
+      group("ポテトを使ったレシピを教えてと送った時のテスト", () {
+        setUpAll(() async {
+          //「ポテトを使ったレシピを教えて」を日本語で
+          promptString = "ポテトを使ったレシピを教えて";
+          final sendedMessage =
+              messageThatUserInputted(promptString, '30', '3');
+          //送信するリクエストボディを作成
+          sendedBodyString =
+              json.encode(getRequestBodyForPaLMApi(sendedMessage));
 
-      test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
-        expect(response.bodyString, contains('recipe_description'));
+          //レスポンスを取得
+          response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+        });
+        test('取得したメッセージが空ではないこと', () async {
+          expect(response, isNotNull);
+        });
+
+        test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_name'));
+        });
+
+        test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_description'));
+        });
       });
     });
 
-    group("世界人口を教えてと送信したときのテスト", () {
-      setUpAll(() async {
-        // 「世界人口を教えて」を英語で
-        promptString = "please tell me the world population";
-        final sendedMessage =
-            messageThatUserInputtedInEnglish(promptString, '30', '3');
-        //送信するリクエストボディを作成
-        sendedBodyString = json.encode(getRequestBodyForPaLMApi(sendedMessage));
+    group("レシピ以外をメッセージとして送信したときのテスト", () {
+      group("あなたの友達は何人かとを送信したときのテスト", () {
+        setUpAll(() async {
+          // 「友達は何人か」を日本語で
+          promptString = "あなたの友達は何人か";
+          final sendedMessage =
+              messageThatUserInputted(promptString, '30', '3');
+          //送信するリクエストボディを作成
+          sendedBodyString =
+              json.encode(getRequestBodyForPaLMApi(sendedMessage));
 
-        //レスポンスを取得
-        response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
-      });
-      test('取得したメッセージが空ではないこと', () async {
-        expect(response, isNotNull);
+          //レスポンスを取得
+          response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+        });
+        test('取得したメッセージが空ではないこと', () async {
+          expect(response, isNotNull);
+        });
+
+        test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_name'));
+        });
+
+        test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
+          expect(response.bodyString, contains('recipe_description'));
+        });
       });
 
-      test('取得したメッセージの一部にrecipe_nameが含まれていること', () async {
-        expect(response.bodyString, contains('recipe_name'));
-      });
+      group("世界人口を教えてと送信したときのテスト", () {
+        setUpAll(() async {
+          // 「世界人口を教えて」を日本語で
+          promptString = "世界人口を教えて";
+          final sendedMessage =
+              messageThatUserInputted(promptString, '30', '3');
+          //送信するリクエストボディを作成
+          sendedBodyString =
+              json.encode(getRequestBodyForPaLMApi(sendedMessage));
 
-      test('取得したメッセージの一部にrecipe_descriptionが含まれていること', () async {
-        expect(response.bodyString, contains('recipe_description'));
+          //レスポンスを取得
+          response = await paLMApiDataSource.getInfo(sendedBodyString, paLMKey);
+        });
+        test('取得したメッセージが空ではないこと', () async {
+          expect(response, isNotNull);
+        });
       });
     });
   });
