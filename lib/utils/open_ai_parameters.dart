f5ef659cc5b24b0133bfe53675356890f34d8145
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-10 16:04:23 +0900

    [update] gpt-3.5から4に変更

--- lib/utils/open_ai_parameters.dart
+++ lib/utils/open_ai_parameters.dart
@@ -1,5 +1,5 @@
 Map<String, dynamic> getOpenAIParameters(String messages) => {
-      'model': 'gpt-3.5-turbo',
+      'model': 'gpt-4',
       'messages': [
         {'role': 'user', 'content': messages}
       ]
