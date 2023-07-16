163745a1d5ff581b6c0273c14c64a52ec66906ec
佐藤 佑哉 <y-sato@sstn.jp>
2023-07-16 23:48:28 +0900

    [add] AuthDataSourceのSupabaseの実装クラスを追加

---
+++ lib/data/sources/remote/supabase_auth_data_source.dart
@@ -0,0 +1,49 @@
+import 'package:cooking_completly_understood/data/sources/interfaces/auth_data_source.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseAuthDataSource implements AuthDataSource {
+  //Supabaseのインスタンスを保持する
+  final SupabaseClient _supabaseClient = Supabase.instance.client;
+  @override
+  Future<String> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isSignedIn() {
+    // TODO: implement isSignedIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signInWithApple() {
+    // TODO: implement signInWithApple
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    // TODO: implement signInWithEmailAndPassword
+    await _supabaseClient.auth
+        .signInWithPassword(email: email, password: password);
+  }
+
+  @override
+  Future<void> signInWithGoogle() {
+    // TODO: implement signInWithGoogle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUpWithEmailAndPassword(String email, String password) async {
+    // TODO: implement signUpWithEmailAndPassword
+    await _supabaseClient.auth.signUp(email: email, password: password);
+  }
+}
