33e24dd081005fadc70708c8178bea85a605be32
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-01 23:50:07 +0900

    [add] FoodEntityをfreezedで実装

--- lib/data/models/food_model/food_entity.dart
+++ lib/data/models/food_model/food_entity.dart
@@ -1,19 +1,14 @@
-import 'package:cooking_completly_understood/domain/models/food/food.dart';
-import 'package:isar/isar.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'food_entity.g.dart';
+part 'food_entity.freezed.dart';
 
-@collection
-class FoodEntity {
-  Id id = Isar.autoIncrement;
-  late String name;
-  late DateTime createdAt;
-
-  Food toFood() {
-    return Food(
-      id: id,
-      name: name,
-      createdAt: createdAt,
-    );
-  }
+@freezed
+class FoodEntity with _$FoodEntity {
+  const factory FoodEntity({
+    required int id,
+    required String foodName,
+    required int count,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+  }) = _FoodEntity;
 }
