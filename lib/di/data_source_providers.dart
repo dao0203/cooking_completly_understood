2d72a7e95fd14e22f9a0840e5092a7d2464382ca
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-01 23:52:33 +0900

    [add] SupabaseFoodDataSourceを実装・DI

--- lib/di/data_source_providers.dart
+++ lib/di/data_source_providers.dart
@@ -3,6 +3,7 @@ import 'package:cooking_completly_understood/data/sources/local/isar_food_data_s
 import 'package:cooking_completly_understood/data/sources/local/isar_my_message_data_source.dart';
 import 'package:cooking_completly_understood/data/sources/local/isar_recipe_data_source.dart';
 import 'package:cooking_completly_understood/data/sources/local/geolocator_position_data_source.dart';
+import 'package:cooking_completly_understood/data/sources/local/supabase_food_data_source.dart';
 import 'package:cooking_completly_understood/data/sources/remote/chopper_open_ai_chat_data_source.dart';
 import 'package:cooking_completly_understood/data/sources/remote/chopper_open_meteo_api_data_source.dart';
 import 'package:cooking_completly_understood/data/sources/remote/palm_api_chat_data_source.dart';
@@ -11,8 +12,7 @@ import 'package:cooking_completly_understood/di/isar_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 //FoodDataSource
-final foodDataSourceProvider = Provider(
-    (ref) => ref.read(isarProvider).then((isar) => IsarFoodDataSource(isar)));
+final foodDataSourceProvider = Provider((ref) => SupabaseFoodDataSource());
 
 //MyMessageDataSource
 final myMessageDataSourceProvider = Provider(((ref) =>
