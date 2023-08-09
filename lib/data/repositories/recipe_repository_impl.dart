13141a5f63a7439a0c3533c37bf11d668516f945
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-10 00:33:39 +0900

    [update] 変更

--- lib/data/repositories/recipe_repository_impl.dart
+++ lib/data/repositories/recipe_repository_impl.dart
@@ -1,4 +1,3 @@
-import 'package:cooking_completly_understood/data/models/recipe_entity/recipe_model.dart';
 import 'package:cooking_completly_understood/data/sources/interfaces/recipe_data_source.dart';
 import 'package:cooking_completly_understood/domain/models/recipe/recipe.dart';
 import 'package:cooking_completly_understood/domain/repositories/recipe_repository.dart';
@@ -7,24 +6,56 @@ class RecipeRepositoryImpl implements RecipeRepository {
   final RecipeDataSource _recipeService;
   RecipeRepositoryImpl(this._recipeService);
   @override
-  Future<void> changeFavoriteStatus(RecipeModel recipeModel) =>
-      _recipeService.changeFavoriteStatus(recipeModel);
+  Future<void> changeFavoriteStatus(Recipe recipe) =>
+      _recipeService.changeFavoriteStatus(recipe.id);
 
   @override
-  Future<void> changeMadeStatus(RecipeModel recipeModel) =>
-      _recipeService.changeMadeStatus(recipeModel);
+  Future<void> changeMadeStatus(Recipe recipe) =>
+      _recipeService.changeMadeStatus(recipe.id);
 
   @override
-  Stream<List<Recipe>> getAllRecipes() => _recipeService
-      .getAll()
-      .map((event) => event.map((e) => e.toRecipe()).toList());
+  Stream<List<Recipe>> getAllRecipes() =>
+      _recipeService.getAll().map((event) => event
+          .map((e) => Recipe(
+              id: e.id,
+              name: e.name,
+              description: e.description,
+              cookingTimeMinutes: e.cookingTimeMinutes,
+              ingredientName: e.ingredientName,
+              ingredientQuantity: e.ingredientQuantity,
+              stepNumber: e.stepNumber,
+              stepDescription: e.stepDescription,
+              calorie: e.calorie,
+              protein: e.protein,
+              fat: e.fat,
+              carbohydrate: e.carbohydrate,
+              salt: e.salt,
+              createdAt: e.createdAt,
+              isMade: e.isMade,
+              isFavorite: e.isFavorite))
+          .toList());
 
   @override
-  Stream<List<Recipe>> getRecipeById(int id) => _recipeService
-      .getById(id)
-      .map((event) => event.map((e) => e.toRecipe()).toList());
+  Stream<Recipe> getRecipeById(int id) =>
+      _recipeService.getById(id).map((event) => Recipe(
+            id: event.id,
+            name: event.name,
+            description: event.description,
+            cookingTimeMinutes: event.cookingTimeMinutes,
+            ingredientName: event.ingredientName,
+            ingredientQuantity: event.ingredientQuantity,
+            stepNumber: event.stepNumber,
+            stepDescription: event.stepDescription,
+            calorie: event.calorie,
+            protein: event.protein,
+            fat: event.fat,
+            carbohydrate: event.carbohydrate,
+            salt: event.salt,
+            createdAt: event.createdAt,
+            isMade: event.isMade,
+            isFavorite: event.isFavorite,
+          ));
 
   @override
-  Future<void> insert(RecipeModel recipeModel) =>
-      _recipeService.insert(recipeModel);
+  Future<void> insert(Recipe recipe) => _recipeService.insert(recipe);
 }
