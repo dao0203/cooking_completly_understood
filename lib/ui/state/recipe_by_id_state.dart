dd683b067754891e72626eb2145b0d430be6650e
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-10 01:17:50 +0900

    [update] UI側の破壊的な変更!!

--- lib/ui/state/recipe_by_id_state.dart
+++ lib/ui/state/recipe_by_id_state.dart
@@ -1,4 +1,3 @@
-import 'package:cooking_completly_understood/data/models/recipe_entity/recipe_model.dart';
 import 'package:cooking_completly_understood/di/repository_providers.dart';
 import 'package:cooking_completly_understood/domain/models/recipe/recipe.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,67 +7,17 @@ part 'recipe_by_id_state.g.dart';
 @riverpod
 class RecipeByIdState extends _$RecipeByIdState {
   @override
-  Future<Stream<List<Recipe>>> build(int id) async {
-    return await ref.read(recipeRepositoryProvider).then(
-      (value) {
-        return value.getRecipeById(id);
-      },
-    );
+  Stream<Recipe> build(int id) {
+    return ref.watch(recipeRepositoryProvider).getRecipeById(id);
   }
 
   //レシピのお気に入り状態を変更するメソッド
   Future<void> changeFavoriteStatus(Recipe recipe) async {
-    await ref.read(recipeRepositoryProvider).then(
-      (value) {
-        return value.changeFavoriteStatus(
-          RecipeModel()
-            ..id = recipe.id
-            ..name = recipe.name
-            ..role = recipe.role
-            ..description = recipe.description
-            ..cookingTime = recipe.cookingTime
-            ..ingredientName = recipe.ingredientName
-            ..ingredientQuantity = recipe.ingredientQuantity
-            ..stepNumber = recipe.stepNumber
-            ..stepDescription = recipe.stepDescription
-            ..calorie = recipe.calorie
-            ..protein = recipe.protein
-            ..fat = recipe.fat
-            ..carbohydrate = recipe.carbohydrate
-            ..salt = recipe.salt
-            ..timeStamp = recipe.timeStamp
-            ..isMade = recipe.isMade
-            ..isFavorite = !recipe.isFavorite,
-        );
-      },
-    );
+    await ref.watch(recipeRepositoryProvider).changeFavoriteStatus(recipe);
   }
 
   //レシピの料理済み状態を変更するメソッド
   Future<void> changeMadeStatus(Recipe recipe) async {
-    await ref.read(recipeRepositoryProvider).then(
-      (value) {
-        return value.changeMadeStatus(
-          RecipeModel()
-            ..id = recipe.id
-            ..name = recipe.name
-            ..role = recipe.role
-            ..description = recipe.description
-            ..cookingTime = recipe.cookingTime
-            ..ingredientName = recipe.ingredientName
-            ..ingredientQuantity = recipe.ingredientQuantity
-            ..stepNumber = recipe.stepNumber
-            ..stepDescription = recipe.stepDescription
-            ..calorie = recipe.calorie
-            ..protein = recipe.protein
-            ..fat = recipe.fat
-            ..carbohydrate = recipe.carbohydrate
-            ..salt = recipe.salt
-            ..timeStamp = recipe.timeStamp
-            ..isMade = !recipe.isMade
-            ..isFavorite = recipe.isFavorite,
-        );
-      },
-    );
+    await ref.watch(recipeRepositoryProvider).changeMadeStatus(recipe);
   }
 }
--- lib/ui/state/recipe_state.dart
+++ lib/ui/state/recipe_state.dart
@@ -1,4 +1,3 @@
-import 'package:cooking_completly_understood/data/models/recipe_entity/recipe_model.dart';
 import 'package:cooking_completly_understood/di/repository_providers.dart';
 import 'package:cooking_completly_understood/domain/models/recipe/recipe.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -8,57 +7,19 @@ part 'recipe_state.g.dart';
 @riverpod
 class RecipeState extends _$RecipeState {
   @override
-  Future<Stream<List<Recipe>>> build() async {
-    return await ref.read(recipeRepositoryProvider).then(
-          (value) => value.getAllRecipes(),
-        );
+  Stream<List<Recipe>> build() {
+    return ref.watch(recipeRepositoryProvider).getAllRecipes();
   }
 
   //レシピのお気に入り状態を変更するメソッド
-  Future<void> changeFavoriteStaus(Recipe recipe) {
-    return ref.read(recipeRepositoryProvider).then(
-          (value) => value.changeFavoriteStatus(RecipeModel()
-            ..id = recipe.id
-            ..name = recipe.name
-            ..role = recipe.role
-            ..description = recipe.description
-            ..cookingTime = recipe.cookingTime
-            ..ingredientName = recipe.ingredientName
-            ..ingredientQuantity = recipe.ingredientQuantity
-            ..stepNumber = recipe.stepNumber
-            ..stepDescription = recipe.stepDescription
-            ..calorie = recipe.calorie
-            ..protein = recipe.protein
-            ..fat = recipe.fat
-            ..carbohydrate = recipe.carbohydrate
-            ..salt = recipe.salt
-            ..timeStamp = recipe.timeStamp
-            ..isMade = recipe.isMade
-            ..isFavorite = !recipe.isFavorite),
-        );
+  Future<void> changeFavoriteStaus(Recipe recipe) async {
+    return await ref
+        .watch(recipeRepositoryProvider)
+        .changeFavoriteStatus(recipe);
   }
 
   //レシピの料理済み状態を変更するメソッド
   Future<void> changeMadeStatus(Recipe recipe) {
-    return ref.read(recipeRepositoryProvider).then(
-          (value) => value.changeMadeStatus(RecipeModel()
-            ..id = recipe.id
-            ..name = recipe.name
-            ..role = recipe.role
-            ..description = recipe.description
-            ..cookingTime = recipe.cookingTime
-            ..ingredientName = recipe.ingredientName
-            ..ingredientQuantity = recipe.ingredientQuantity
-            ..stepNumber = recipe.stepNumber
-            ..stepDescription = recipe.stepDescription
-            ..calorie = recipe.calorie
-            ..protein = recipe.protein
-            ..fat = recipe.fat
-            ..carbohydrate = recipe.carbohydrate
-            ..salt = recipe.salt
-            ..timeStamp = recipe.timeStamp
-            ..isMade = !recipe.isMade
-            ..isFavorite = recipe.isFavorite),
-        );
+    return ref.watch(recipeRepositoryProvider).changeMadeStatus(recipe);
   }
 }
