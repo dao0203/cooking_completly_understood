0a0d39607f475191b3beb56cdc0ca1cfe0a13171
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-09 12:31:32 +0900

    [add] MyMesesageの引数を変更　

--- lib/domain/repositories/my_message_repository.dart
+++ lib/domain/repositories/my_message_repository.dart
@@ -1,7 +1,6 @@
-import 'package:cooking_completly_understood/data/models/my_message_model/my_message_model.dart';
 import 'package:cooking_completly_understood/domain/models/my_message/my_message.dart';
 
 abstract class MyMessageRepository {
   Stream<List<MyMessage>> getAll();
-  Future<void> insert(MyMessageModel myMessageModel);
+  Future<void> insert(String myMessage);
 }
