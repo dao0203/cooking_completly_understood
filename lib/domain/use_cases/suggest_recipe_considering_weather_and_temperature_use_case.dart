c2ea100922bd15d52579ee5eee2200acb55af3d0
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-10 03:21:55 +0900

    [add] UseCaseをふるまいごとにプライベートメソッドに実装

--- lib/domain/use_cases/suggest_recipe_considering_weather_and_temperature_use_case.dart
+++ lib/domain/use_cases/suggest_recipe_considering_weather_and_temperature_use_case.dart
@@ -10,6 +10,7 @@ import 'package:cooking_completly_understood/domain/repositories/weather_reposit
 import 'package:cooking_completly_understood/domain/use_cases/use_case.dart';
 import 'package:cooking_completly_understood/utils/constants.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_mlkit_translation/google_mlkit_translation.dart';
 
 class SuggestRecipeConsideringWeatherAndTemperatureUseCase
     implements UseCase<String, Future<void>> {
@@ -28,14 +29,72 @@ class SuggestRecipeConsideringWeatherAndTemperatureUseCase
   );
   @override
   Future<void> call(String inputedMessage) async {
-    //----------マイメッセージをローカルDBに保存----------
-
+    //マイメッセージをローカルDBに保存
     await _myMessageRepository.insert(inputedMessage);
+    //現在地を取得
+    final position = await _getCurrentPosition();
+    //天気情報を取得
+    final currentWeatherInfo = await _weatherRepository.getCurrentWeather(
+        position.latitude, position.longitude);
+    // メッセージを英語に変換 PaLM API用
+    // final String translatedMessage =
+    //     await _translateEnglishToJapanese(inputedMessage);
 
-    //----------現在地を取得するための手続き----------
+    //送信するメッセージを作成
+    final String sendedMessage = createMessageThatUserInputted(
+      inputedMessage,
+      currentWeatherInfo.temperature.toString(),
+      currentWeatherInfo.weatherCode.toString(),
+    );
 
-    // 位置情報を取得できるかどうかのフラグ
-    await Geolocator.isLocationServiceEnabled()
+    //送信するメッセージをBodyに変えてエンコード
+    // final String encodedBody =
+    // json.encode(getRequestBodyForPaLMApi(sendedMessage));  //PaLM API用
+    // json.encode(getOpenAIParameters(sendedMessage)); //OpenAI用
+    //メッセージを取得
+
+    final message =
+        await _messageRepository.sendAndReceiveMessage(sendedMessage);
+
+    //PaLM API用
+    // //PaLM APIだとMarkDown形式で返ってくるので、変換する
+    // final convertedMessage =
+    //     message.substring(message.indexOf('{') + 1, message.length - 1);
+    // //メッセージをパース
+    // final recipeResponse =
+    //     RecipeResponse.fromJson(json.decode(convertedMessage));
+    //OpenAI時の処理
+
+    //メッセージをパース
+    final recipeResponse = RecipeResponse.fromJson(json.decode(message));
+    //レシピを保存するために、レシピモデルに変換
+    final insertedRecipe = Recipe(
+      id: 0,
+      name: recipeResponse.recipeName,
+      description: recipeResponse.recipeDescription,
+      cookingTimeMinutes: recipeResponse.recipeCookingTime,
+      ingredientNames:
+          recipeResponse.recipeIngredients.map((e) => e.name).toList(),
+      ingredientQuantities:
+          recipeResponse.recipeIngredients.map((e) => e.quantity).toList(),
+      stepNumbers: recipeResponse.recipeSteps.map((e) => e.number).toList(),
+      stepDescriptions:
+          recipeResponse.recipeSteps.map((e) => e.description).toList(),
+      calorie: double.parse(recipeResponse.nutrition.calorie),
+      protein: double.parse(recipeResponse.nutrition.protein),
+      fat: double.parse(recipeResponse.nutrition.fat),
+      carbohydrate: double.parse(recipeResponse.nutrition.carbohydrate),
+      salt: double.parse(recipeResponse.nutrition.salt),
+      createdAt: DateTime.now(),
+      isMade: false,
+      isFavorite: false,
+    );
+    //レシピを保存
+    await _recipeRepository.insert(insertedRecipe);
+  }
+
+  Future<Position> _getCurrentPosition() async {
+    return await Geolocator.isLocationServiceEnabled()
         .then((isLocationServiceEnabled) async {
       if (!isLocationServiceEnabled) {
         // 位置情報を取得できない場合は、エラーを投げる
@@ -56,71 +115,25 @@ class SuggestRecipeConsideringWeatherAndTemperatureUseCase
           });
         }
       });
-
       //現在地を取得
-      final position = await _positionRepository.getCurrentPosition();
-
-      //天気情報を取得
-      final currentWeatherInfo = await _weatherRepository.getCurrentWeather(
-          position.latitude, position.longitude);
-
-      //メッセージを英語に変換 PaLM API用
-      // final String translatedMessage = await OnDeviceTranslator(
-      //   sourceLanguage: TranslateLanguage.japanese,
-      //   targetLanguage: TranslateLanguage.english,
-      // ).translateText(inputedMessage);
-
-      //送信するメッセージを作成
-      final String sendedMessage = messageThatUserInputted(
-        inputedMessage,
-        currentWeatherInfo.temperature.toString(),
-        currentWeatherInfo.weatherCode.toString(),
-      );
-
-      //送信するメッセージをBodyに変えてエンコード
-      // final String encodedBody =
-      // json.encode(getRequestBodyForPaLMApi(sendedMessage));  //PaLM API用
-      // json.encode(getOpenAIParameters(sendedMessage)); //OpenAI用
-      //メッセージを取得
-
-      final message =
-          await _messageRepository.sendAndReceiveMessage(sendedMessage);
-
-      //PaLM API用
-      // //PaLM APIだとMarkDown形式で返ってくるので、変換する
-      // final convertedMessage =
-      //     message.substring(message.indexOf('{') + 1, message.length - 1);
-      // //メッセージをパース
-      // final recipeResponse =
-      //     RecipeResponse.fromJson(json.decode(convertedMessage));
-      //OpenAI時の処理
-
-      //メッセージをパース
-      final recipeResponse = RecipeResponse.fromJson(json.decode(message));
-      //レシピを保存するために、レシピモデルに変換
-      final insertedRecipe = Recipe(
-        id: 0,
-        name: recipeResponse.recipeName,
-        description: recipeResponse.recipeDescription,
-        cookingTimeMinutes: recipeResponse.recipeCookingTime,
-        ingredientNames:
-            recipeResponse.recipeIngredients.map((e) => e.name).toList(),
-        ingredientQuantities:
-            recipeResponse.recipeIngredients.map((e) => e.quantity).toList(),
-        stepNumbers: recipeResponse.recipeSteps.map((e) => e.number).toList(),
-        stepDescriptions:
-            recipeResponse.recipeSteps.map((e) => e.description).toList(),
-        calorie: double.parse(recipeResponse.nutrition.calorie),
-        protein: double.parse(recipeResponse.nutrition.protein),
-        fat: double.parse(recipeResponse.nutrition.fat),
-        carbohydrate: double.parse(recipeResponse.nutrition.carbohydrate),
-        salt: double.parse(recipeResponse.nutrition.salt),
-        createdAt: DateTime.now(),
-        isMade: false,
-        isFavorite: false,
-      );
-      //レシピを保存
-      await _recipeRepository.insert(insertedRecipe);
+      return await _positionRepository.getCurrentPosition();
     });
+  }
+
+  Future<String> _translateEnglishToJapanese(String englishMessage) async {
+    //英語を日本語に変換
+    return await OnDeviceTranslator(
+      sourceLanguage: TranslateLanguage.english,
+      targetLanguage: TranslateLanguage.japanese,
+    ).translateText(englishMessage);
+  }
+
+  //送信する用のメッセージを作成
+  String createMessageThatUserInputted(
+    String inputedMessage,
+    String temperature,
+    String weatherCode,
+  ) {
+    return messageThatUserInputted(inputedMessage, temperature, weatherCode);
   }
 }
