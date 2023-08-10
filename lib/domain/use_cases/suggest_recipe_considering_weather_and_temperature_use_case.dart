c592123626e2e5076313fd8fcb9bce35434e8796
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-10 13:27:51 +0900

    [update] レシピ提案のユースケースをふるまいごとにメソッドに分けた

--- lib/domain/use_cases/suggest_recipe_considering_weather_and_temperature_use_case.dart
+++ lib/domain/use_cases/suggest_recipe_considering_weather_and_temperature_use_case.dart
@@ -41,54 +41,28 @@ class SuggestRecipeConsideringWeatherAndTemperatureUseCase
     //     await _translateEnglishToJapanese(inputedMessage);
 
     //送信するメッセージを作成
-    final String sendedMessage = createMessageThatUserInputted(
-      inputedMessage,
-      currentWeatherInfo.temperature.toString(),
-      currentWeatherInfo.weatherCode.toString(),
-    );
+    final String sendedMessage = _createMessageThatUserInputted(
+        inputedMessage,
+        currentWeatherInfo.temperature.toString(),
+        currentWeatherInfo.weatherCode.toString());
 
     //送信するメッセージをBodyに変えてエンコード
     // final String encodedBody =
     // json.encode(getRequestBodyForPaLMApi(sendedMessage));  //PaLM API用
     // json.encode(getOpenAIParameters(sendedMessage)); //OpenAI用
-    //メッセージを取得
 
+    //メッセージを取得
     final message =
         await _messageRepository.sendAndReceiveMessage(sendedMessage);
 
     //PaLM API用
     // //PaLM APIだとMarkDown形式で返ってくるので、変換する
-    // final convertedMessage =
-    //     message.substring(message.indexOf('{') + 1, message.length - 1);
-    // //メッセージをパース
-    // final recipeResponse =
-    //     RecipeResponse.fromJson(json.decode(convertedMessage));
-    //OpenAI時の処理
+    // final convertedMessage = _convertMessageFromPaLM(message);
 
     //メッセージをパース
     final recipeResponse = RecipeResponse.fromJson(json.decode(message));
     //レシピを保存するために、レシピモデルに変換
-    final insertedRecipe = Recipe(
-      id: 0,
-      name: recipeResponse.recipeName,
-      description: recipeResponse.recipeDescription,
-      cookingTimeMinutes: recipeResponse.recipeCookingTime,
-      ingredientNames:
-          recipeResponse.recipeIngredients.map((e) => e.name).toList(),
-      ingredientQuantities:
-          recipeResponse.recipeIngredients.map((e) => e.quantity).toList(),
-      stepNumbers: recipeResponse.recipeSteps.map((e) => e.number).toList(),
-      stepDescriptions:
-          recipeResponse.recipeSteps.map((e) => e.description).toList(),
-      calorie: double.parse(recipeResponse.nutrition.calorie),
-      protein: double.parse(recipeResponse.nutrition.protein),
-      fat: double.parse(recipeResponse.nutrition.fat),
-      carbohydrate: double.parse(recipeResponse.nutrition.carbohydrate),
-      salt: double.parse(recipeResponse.nutrition.salt),
-      createdAt: DateTime.now(),
-      isMade: false,
-      isFavorite: false,
-    );
+    final insertedRecipe = _convertRecipeResponseToRecipe(recipeResponse);
     //レシピを保存
     await _recipeRepository.insert(insertedRecipe);
   }
@@ -129,11 +103,41 @@ class SuggestRecipeConsideringWeatherAndTemperatureUseCase
   }
 
   //送信する用のメッセージを作成
-  String createMessageThatUserInputted(
+  String _createMessageThatUserInputted(
     String inputedMessage,
     String temperature,
     String weatherCode,
   ) {
     return messageThatUserInputted(inputedMessage, temperature, weatherCode);
+  }
+
+  //PaLM APIだとMarkDown形式で返ってくるので、変換する振る舞いを書きたいので、おすすめのメソッド名を教えてください
+  String _convertMessageFromPaLM(String message) {
+    return message.substring(message.indexOf('{') + 1, message.length - 1);
+  }
+
+  // RecipeResponseを保存用のRecipeに変換するメソッドを書きたいので、おすすめのメソッド名を教えてください
+  Recipe _convertRecipeResponseToRecipe(RecipeResponse recipeResponse) {
+    return Recipe(
+      id: 0,
+      name: recipeResponse.recipeName,
+      description: recipeResponse.recipeDescription,
+      cookingTimeMinutes: recipeResponse.recipeCookingTime,
+      ingredientNames:
+          recipeResponse.recipeIngredients.map((e) => e.name).toList(),
+      ingredientQuantities:
+          recipeResponse.recipeIngredients.map((e) => e.quantity).toList(),
+      stepNumbers: recipeResponse.recipeSteps.map((e) => e.number).toList(),
+      stepDescriptions:
+          recipeResponse.recipeSteps.map((e) => e.description).toList(),
+      calorie: double.parse(recipeResponse.nutrition.calorie),
+      protein: double.parse(recipeResponse.nutrition.protein),
+      fat: double.parse(recipeResponse.nutrition.fat),
+      carbohydrate: double.parse(recipeResponse.nutrition.carbohydrate),
+      salt: double.parse(recipeResponse.nutrition.salt),
+      createdAt: DateTime.now(),
+      isMade: false,
+      isFavorite: false,
+    );
   }
 }
